f3dd243e5801f493551b473af7e36f04bed40828
Graeme Fulton <graemefulton89@gmail.com>
2022-02-28 01:34:03 +0100

    mobile menu

--- components/navbar.js
+++ components/navbar.js
@@ -1,42 +1,31 @@
 import Link from 'next/link';
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 export default function Navbar({ posts, type, activeNav }) {
 
     return(
-    // <!-- This example requires Tailwind CSS v2.0+ -->
-    <nav className="bg-white shadow fixed w-full top-0 z-50">
+    <Disclosure as="nav" className="bg-white shadow fixed w-full top-0 z-50">
+      {({ open }) => (
+          <>
+    {/* // <!-- This example requires Tailwind CSS v2.0+ --> */}
     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* <!-- Mobile menu button--> */}
-            <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-            {/* <!--
-                Icon when menu is closed.
-
-                Heroicon name: outline/menu
-
-                Menu open: "hidden", Menu closed: "block"
-            --> */}
-            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-            {/* <!--
-                Icon when menu is open.
-
-                Heroicon name: outline/x
-
-                Menu open: "block", Menu closed: "hidden"
-            --> */}
-            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-            </button>
+            <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
         </div>
         <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <Link href="/" as="/">
                 <div className="flex-shrink-0 flex items-center cursor-pointer">
-                        <img className="block lg:hidden h-8 w-auto" src="https://prototypr.io/wp-content/uploads/2018/10/1_SdMNHdB-vvUH4eWMBYkzQw.png" alt="Workflow"/>
+                        <img className="block lg:hidden h-12 w-auto" src="https://prototypr.io/wp-content/uploads/2018/10/1_SdMNHdB-vvUH4eWMBYkzQw.png" alt="Workflow"/>
                         <img className="hidden lg:block h-8 w-auto" src="https://letter-so.s3.us-west-1.amazonaws.com/uploads/LQatPZxS8qFXER9Hi/2rzv1a" alt="Workflow"/>
                 </div>
             </Link>
@@ -85,20 +74,48 @@ export default function Navbar({ posts, type, activeNav }) {
     </div>
 
     {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-    <div className="sm:hidden" id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1">
-        {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-800 hover:bg-gray-100 hover:text-gray-900" --> */}
-        <a href="#" className={activeNav=='become-author'?"bg-gray-100 text-gray-900 block px-3 py-2 rounded-md text-base font-medium":"text-gray-800 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"} aria-current="page">Become an author</a>
-
-        <a href="#" className={activeNav=='subscribe'?"bg-gray-100 text-gray-900 block px-3 py-2 rounded-md text-base font-medium":"text-gray-800 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"}>Subscribe</a>
-
-        {/* <a href="#" className="text-gray-800 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-
-        <a href="#" className="text-gray-800 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium">Calendar</a> */}
-        </div>
-    </div>
-    </nav>
-
+    <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+                <Disclosure.Button
+                  key={'stories'}
+                  as="a"
+                  href={'/posts'}
+                  className={activeNav=='posts'?"bg-gray-100 text-gray-900 block px-3 py-2 rounded-md text-base font-medium":"text-gray-800 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"}
+                  aria-current={'page'}
+                >
+                  Stories
+                </Disclosure.Button>
+                <Disclosure.Button
+                  key={'toolbox'}
+                  as="a"
+                  href={'/toolbox'}
+                  className={activeNav=='toolbox'?"bg-gray-100 text-gray-900 block px-3 py-2 rounded-md text-base font-medium":"text-gray-800 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"}
+                  aria-current={'page'}
+                >
+                  Toolbox
+                </Disclosure.Button>
+                <Disclosure.Button
+                  key={'become-author'}
+                  as="a"
+                  href={'#'}
+                  className={activeNav=='become-author'?"bg-gray-100 text-gray-900 block px-3 py-2 rounded-md text-base font-medium":"text-gray-800 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"}
+                  aria-current={'page'}
+                >
+                  Become an author
+                </Disclosure.Button>
+                <Disclosure.Button
+                  key={'subscribe'}
+                  as="a"
+                  href={'#'}
+                  className={activeNav=='become-author'?"bg-gray-100 text-gray-900 block px-3 py-2 rounded-md text-base font-medium":"text-gray-800 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"}
+                  aria-current={'page'}
+                >
+                  Subscribe
+                </Disclosure.Button>
+            </div>
+          </Disclosure.Panel>
+</>)}
+    </Disclosure>
     )
 
 }
