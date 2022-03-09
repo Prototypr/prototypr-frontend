8b93f5d3d22d52991f09a981582799532b4faac3
Graeme Fulton <graemefulton89@gmail.com>
2022-03-09 02:15:23 +0100

    active nav prop

--- pages/toolbox/[slug].js
+++ pages/toolbox/[slug].js
@@ -19,7 +19,7 @@ export default function Post({ post, morePosts, preview }) {
     return <ErrorPage statusCode={404} />
   }
   return (
-    <Layout preview={preview}>
+    <Layout activeNav={"toolbox"} preview={preview}>
       <Container>
         <Header />
         {router.isFallback ? (
