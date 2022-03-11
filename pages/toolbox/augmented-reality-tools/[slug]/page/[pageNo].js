a0676aee0696fc558f6a016d83691c1b1d92bd1a
wendyjisung <wendyjisung@aliyun.com>
2022-03-11 17:17:03 +0800

    feat:/toolbox/augmented-reality-tools/[slug]/page/[pageNo]

--- pages/toolbox/augmented-reality-tools/[slug]/page/[pageNo].js
+++ pages/toolbox/augmented-reality-tools/[slug]/page/[pageNo].js
@@ -6,28 +6,54 @@ import MoreStories from '@/components/more-stories'
 import NewPagination from '@/components/pagination'
 import FilterCategory from '@/components/FilterCategory'
 
-import { getAllPostsForToolsPage, getPostsByPageForToolsPage } from '@/lib/api'
+// import { getAllPostsForToolsPage, getPostsByPageForToolsPage } from '@/lib/api'
+import { getAllPostsForToolsSubcategoryPage, getPostsByPageForToolsSubcategoryPage } from '@/lib/api'
 import Link from 'next/link'
 const PAGE_SIZE = 13;
 const ALL_SLUGS = [{
-    key: "analysis",
-    name: "User Analysis"
+    key: "3d",
+    name: "3D",
+    tags: ["3d"]
 },{
-    key: "journey",
-    name: "User Journey"
+    key: "api",
+    name: "API",
+    tags: ["api", "vr"]
 },{
-    key: "research",
-    name: "User Research"
+    key: "augmented_reality",
+    name: "Augmented Reality",
+    tags: ["ar", "augmented-reality"]
+},{
+    key: "curated_resources",
+    name: "Curated Resources",
+    tags: ["vr", "resource"]
+},{
+    key: "platform",
+    name: "Platform",
+    tags: ["tool", "vr"]
+},{
+    key: "viewer",
+    name: "Viewer",
+    tags: ["viewer"]
+},{
+    key: "virtual_reality",
+    name: "Virtual Reality",
+    tags: ["vr", "virtual-reality"]
 }]
 
 
-export default function ToolboxPage({allPosts = [], preview, pagination}) {
-    //pagination is like {"total":1421,"pageSize":12,"page":2,"pageCount":119}
+
+export default function ToolboxPage({allPosts = [], preview, pagination,slug}) {
+    //pagination is like {"total":48,"pageSize":13,"page":1,"pageCount":4}
     const router = useRouter()
 
     const [selectedFilter, setSelectedFilter] = useState("")
     const onPageNumChange = (pageNo) => {
-        router.push(`/toolbox/page/${pageNo}`)
+        router.push({
+            pathname:`/toolbox/augmented-reality-tools/[slug]/page/${pageNo}`,
+            query: {
+                slug
+            }
+        })
       }
 
     return (
@@ -40,14 +66,16 @@ export default function ToolboxPage({allPosts = [], preview, pagination}) {
                         <div className='w-full h-screen  flex flex-col'>
                         <h1 className="font-semibold text-xl my-4">All Tools</h1>
                         <div className="display-none mb-8 lg:block text-gray-800">
+
                             <div className="px-2">
                                 <h1 className="font-semibold pb-2 mb-2 border-b border-gray-300 pr-3 text-xs uppercase text-gray-900">UX Tools</h1>
                             </div>
                             {
                                 ALL_SLUGS && ALL_SLUGS.map((item, index) => {
                                     return (
-                                        <div className="cursor-pointer text-sm" key={`toobox_cat_${index}`}>
-                                            <Link href={`/toolbox/${item.key}/page/1`}><div className="text-gray-700 hover:text-blue-500 p-2 rounded">
+                                        <div className="cursor-pointer text-sm" key={`toolbox_${slug}_cat_${index}`}>
+                                            <Link href={`/toolbox/augmented-reality-tools/${item.key}/page/1`}>
+                                                <div className={`text-gray-700 hover:text-blue-500 p-2 rounded ${item.key === slug ? ' text-blue-600 font-semibold' : ''}`}>
                                                 {item.name}
                                                 </div>
                                             </Link>
@@ -55,10 +83,6 @@ export default function ToolboxPage({allPosts = [], preview, pagination}) {
                                     )
                                 })
                             }
-                            
-                            <Link href="/toolbox/ux-tools/page/1">
-                                <a className="inline-block text-blue-600 my-2 text-sm px-2">Browse all UX →</a>
-                            </Link>
                         </div>
                     </div>
                        
@@ -82,26 +106,35 @@ export default function ToolboxPage({allPosts = [], preview, pagination}) {
 
 export async function getStaticProps({ preview = null, params}) {
     const pageSize = PAGE_SIZE
-    const page = params.pageNo
-    const allPosts = (await getPostsByPageForToolsPage(preview, pageSize, page )) || []
-    
+    const {pageNo, slug} = params;
+    const foundSlug = ALL_SLUGS.find((SLUG, index) => {
+        return slug === SLUG.key
+    })
+    const allPosts = (await getPostsByPageForToolsSubcategoryPage(preview, pageSize, pageNo, foundSlug.tags )) || []
+    // console.log('page info**********' + JSON.stringify(allPosts.meta.pagination))
     const pagination = allPosts.meta.pagination
     return {
         props: {
-            allPosts: allPosts.data, preview, pagination
+            allPosts: allPosts.data, preview, pagination,slug
         },
     }
   }
 
 export async function getStaticPaths() {
-    const allPosts = (await getAllPostsForToolsPage(null, PAGE_SIZE, 0)) || []
-    const pagination = allPosts.meta.pagination
-    const pageCount = pagination.pageCount
-    const pageCountArr = new Array(pageCount).fill(' ');
+    let pageCountRes = 0;
+    let pageCountArr = [];
+    ALL_SLUGS.map(async (item, index)  => {
+        const allPosts = (await getAllPostsForToolsSubcategoryPage(null, PAGE_SIZE, 0, item.tags)) || []
+        const pagination = allPosts.meta.pagination
+        const pageCount = pagination.pageCount
+        let arr = new Array(pageCount).fill('');
+        let newArr = arr.map((i,index) => {
+            return `toolbox/augmented-reality-tools/${item.key}/page/${index+1}`
+        })
+        pageCountArr = pageCountArr.concat(newArr)
+    })
     return {
-        paths: pageCountArr && pageCountArr.map((pageNo) => {
-            return `/toolbox/page/${pageNo}`
-        }) || [],
+        paths: pageCountArr || [],
         fallback: true,
     }
 }
