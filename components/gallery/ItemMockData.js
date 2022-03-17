6656367fc0767f721d47d3a6fce9e468183b8c12
wendyjisung <wendyjisung@aliyun.com>
2022-03-17 16:08:32 +0800

    feat: the Swiper gallery component in toolbox/[slug] page

---
+++ components/gallery/ItemMockData.js
@@ -0,0 +1,102 @@
+const MOCK_UP_ITEM = {
+	"content": "<p>Feeling like user testing does not always give you the insights you need? Testing your prototypes with Useberry, you get not only Session Recordings but also extensive UX insights with Heatmaps, Time Bars, Answers, and User Flows to build better experiences for your users.</p>\n",
+	"databaseId": 122953,
+	"author": {
+		"node": {
+			"extraProfileFields": {
+				"role": "Social Media and Partnerships Management at Prototypr.io"
+			},
+			"avatar": {
+				"url": "https://prototypr.io/wp-content/uploads/2020/08/IMG-20171221-WA0002-1.jpg"
+			},
+			"name": "Sofia Marques",
+			"firstName": "Sofia",
+			"lastName": "Marques",
+			"nicename": null,
+			"slug": "sofia",
+			"description": "👋 marketing &amp; communications specialist. 🎹 artist &amp; music producer (aka mema.). 😋 food lover, building Little Corner 🦄 so many dreams, so little time"
+		}
+	},
+	"title": "Useberry",
+	"link": "https://prototypr.io/toolbox/useberry/",
+	"featuredImage": {
+		"node": {
+			"medium": "https://prototypr.io/wp-content/uploads/2021/01/Useberry-768x576.png",
+			"thumb": "https://prototypr.io/wp-content/uploads/2021/01/Useberry-150x150.png"
+		}
+	},
+	"postLikes": {
+		"likes": null
+	},
+	"toolbox": {
+		"productGallery": "[\"/wp-content/uploads/2021/01/Useberry.png\",\"/wp-content/uploads/2021/01/prototyprio_toolbox_images_05.png\",\"/wp-content/uploads/2021/01/prototyprio_toolbox_images_06.png\",\"/wp-content/uploads/2021/01/prototyprio_toolbox_images_04.png\",\"/wp-content/uploads/2021/01/prototyprio_toolbox_images_03.png\",\"/wp-content/uploads/2021/01/prototyprio_toolbox_images_02.png\"]",
+		"logoNew": {
+			"thumbnail": "https://prototypr.io/wp-content/uploads/2021/01/0IOb55mC-150x150.png"
+		},
+		"video": null,
+		"link": "https://www.useberry.com/?source=prototypr",
+		"imgUrl": "https://prototypr.io/wp-content/uploads/2021/01/Useberry.png",
+		"gallery": [{
+			"medium": "https://prototypr.io/wp-content/uploads/2021/01/Useberry-768x576.png",
+			"thumb": "https://prototypr.io/wp-content/uploads/2021/01/Useberry-150x150.png",
+			"srcSet": "https://prototypr.io/wp-content/uploads/2021/01/Useberry-300x225.png 300w, https://prototypr.io/wp-content/uploads/2021/01/Useberry-1024x768.png 1024w, https://prototypr.io/wp-content/uploads/2021/01/Useberry-768x576.png 768w, https://prototypr.io/wp-content/uploads/2021/01/Useberry.png 1400w"
+		}, {
+			"medium": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_05-768x539.png",
+			"thumb": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_05-150x150.png",
+			"srcSet": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_05-300x210.png 300w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_05-1024x718.png 1024w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_05-768x539.png 768w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_05-1536x1077.png 1536w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_05.png 1540w"
+		}, {
+			"medium": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_06-768x539.png",
+			"thumb": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_06-150x150.png",
+			"srcSet": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_06-300x210.png 300w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_06-1024x718.png 1024w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_06-768x539.png 768w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_06-1536x1077.png 1536w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_06.png 1540w"
+		}, {
+			"medium": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_04-768x539.png",
+			"thumb": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_04-150x150.png",
+			"srcSet": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_04-300x210.png 300w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_04-1024x718.png 1024w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_04-768x539.png 768w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_04-1536x1077.png 1536w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_04.png 1540w"
+		}, {
+			"medium": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_03-768x539.png",
+			"thumb": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_03-150x150.png",
+			"srcSet": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_03-300x210.png 300w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_03-1024x718.png 1024w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_03-768x539.png 768w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_03-1536x1077.png 1536w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_03.png 1540w"
+		}, {
+			"medium": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_02-768x539.png",
+			"thumb": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_02-150x150.png",
+			"srcSet": "https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_02-300x210.png 300w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_02-1024x718.png 1024w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_02-768x539.png 768w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_02-1536x1077.png 1536w, https://prototypr.io/wp-content/uploads/2021/01/prototyprio_toolbox_images_02.png 1540w"
+		}]
+	},
+	"seo": {
+		"metaRobotsNoindex": "index",
+		"metaRobotsNofollow": "follow",
+		"breadcrumbs": [{
+			"text": "Home",
+			"url": "https://prototypr.io/"
+		}, {
+			"text": "Toolbox",
+			"url": "https://prototypr.io/toolbox/"
+		}, {
+			"text": "Useberry",
+			"url": "https://prototypr.io/toolbox/useberry/"
+		}],
+		"canonical": "https://prototypr.io/toolbox/useberry/",
+		"metaDesc": "Feeling like user testing does not always give you the insights you need? Useberry gives you user feedback and rich insights in minutes!",
+		"opengraphDescription": "Feeling like user testing does not always give you the insights you need? Useberry gives you user feedback and rich insights in minutes!",
+		"opengraphModifiedTime": "2021-01-13T21:42:14+00:00",
+		"opengraphPublishedTime": "",
+		"opengraphTitle": "Useberry - User Testing Feedback and Rich Insights in minutes",
+		"opengraphUrl": "https://prototypr.io/toolbox/useberry/",
+		"opengraphImage": null,
+		"schema": {
+			"raw": "{\"@context\":\"https://schema.org\",\"@graph\":[{\"@type\":\"Organization\",\"@id\":\"https://prototypr.io/#organization\",\"name\":\"Prototypr.io\",\"url\":\"https://prototypr.io/\",\"sameAs\":[\"http://facebook.com/prototyprio\",\"http://instagram.com/prototyprio\",\"https://twitter.com/prototypr\"],\"logo\":{\"@type\":\"ImageObject\",\"@id\":\"https://prototypr.io/#logo\",\"inLanguage\":\"en-US\",\"url\":\"https://prototypr-media.sfo2.digitaloceanspaces.com/wp-content/uploads/2016/07/13141841/login-site-logo-1.png\",\"width\":150,\"height\":150,\"caption\":\"Prototypr.io\"},\"image\":{\"@id\":\"https://prototypr.io/#logo\"}},{\"@type\":\"WebSite\",\"@id\":\"https://prototypr.io/#website\",\"url\":\"https://prototypr.io/\",\"name\":\"Prototypr | Prototyping\",\"description\":\"Curated design news: prototyping tools, stories, and news on UX Design and beyond.\",\"publisher\":{\"@id\":\"https://prototypr.io/#organization\"},\"potentialAction\":[{\"@type\":\"SearchAction\",\"target\":\"https://prototypr.io/?s={search_term_string}\",\"query-input\":\"required name=search_term_string\"}],\"inLanguage\":\"en-US\"},{\"@type\":\"ImageObject\",\"@id\":\"https://prototypr.io/toolbox/useberry/#primaryimage\",\"inLanguage\":\"en-US\",\"url\":\"https://prototypr.io/wp-content/uploads/2021/01/Useberry.png\",\"width\":1400,\"height\":1050},{\"@type\":\"WebPage\",\"@id\":\"https://prototypr.io/toolbox/useberry/#webpage\",\"url\":\"https://prototypr.io/toolbox/useberry/\",\"name\":\"Useberry - User Testing Feedback and Rich Insights in minutes\",\"isPartOf\":{\"@id\":\"https://prototypr.io/#website\"},\"primaryImageOfPage\":{\"@id\":\"https://prototypr.io/toolbox/useberry/#primaryimage\"},\"datePublished\":\"2021-01-11T18:27:59+00:00\",\"dateModified\":\"2021-01-13T21:42:14+00:00\",\"description\":\"Feeling like user testing does not always give you the insights you need? Useberry gives you user feedback and rich insights in minutes!\",\"inLanguage\":\"en-US\",\"potentialAction\":[{\"@type\":\"ReadAction\",\"target\":[\"https://prototypr.io/toolbox/useberry/\"]}]}]}"
+		},
+		"title": "Useberry - User Testing Feedback and Rich Insights in minutes",
+		"twitterDescription": "",
+		"twitterImage": null,
+		"twitterTitle": ""
+	},
+	"tags": {
+		"nodes": [{
+			"slug": "user-testing"
+		}]
+	},
+	"meta": []
+};
+
+export default MOCK_UP_ITEM;
---
+++ components/gallery/PopupGallery.js
@@ -0,0 +1,93 @@
+import React, { useState, useRef, useEffect } from "react";
+import SwiperGallery from "./SwiperGallery";
+
+
+export default function PopupGallery({ item, link, img, body, rounded, arrows }) {
+
+  const [desc, setDesc] = useState("");
+//   const [bodyTxt, setBodyTxt] = useState(false);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [galleryComponent, setGalleryComponent] = useState(null);
+
+  const _buildGallery = () => {
+    let PHOTO_SET = [];
+    if (item && item.toolbox) {
+      if (item.toolbox.gallery && item.toolbox.gallery.length) {
+        item.toolbox.gallery.forEach((galleryItem, index) => {
+          PHOTO_SET.push({
+            thumbnail:
+              galleryItem.thumb.indexOf("https://") == -1
+                ? "https://prototypr.gumlet.com" + galleryItem.thumb
+                : galleryItem.thumb,
+            original:
+              galleryItem.medium.indexOf("https://") == -1
+                ? "https://prototypr.gumlet.com" + galleryItem.medium
+                : galleryItem.medium,
+            originalAlt: "Screenshot of product",
+            thumbnailAlt: "Smaller procut screenshot thumbnail",
+            type: "image",
+            srcSet: galleryItem.srcSet,
+            sizes: galleryItem.sizes,
+          });
+        });
+      }
+      if (item.toolbox.video) {
+        const youtubeEmbedLink = item.toolbox.video.replace(
+          "watch?v=",
+          "embed/"
+        );
+        PHOTO_SET.unshift({
+          thumbnail:
+            "https://img.youtube.com/vi/" +
+            item.toolbox.video.split("watch?v=")[1] +
+            "/default.jpg",
+          original:
+            "https://img.youtube.com/vi/" +
+            item.toolbox.video.split("watch?v=")[1] +
+            "/default.jpg",
+          embedUrl: youtubeEmbedLink,
+          renderItem: this._renderVideo.bind(this),
+          originalAlt: "YouTube product video",
+          thumbnailAlt: "YouTube product video",
+          type: "video",
+        });
+      }
+
+      return (
+        <div className="mx-4">
+            {
+                PHOTO_SET && PHOTO_SET.length && <SwiperGallery data={PHOTO_SET} />
+            }
+        </div>
+      );
+    }
+  };
+
+  useEffect(() => {
+    // let bodyTxt = "";
+    // if (process.browser) {
+    //   //extract product description from main stuff
+    //   let temporalDivElement = document.createElement("div");
+    //   // Set the HTML content with the providen
+    //   temporalDivElement.innerHTML = body;
+    //   let bod = temporalDivElement.getElementsByTagName("h2");
+    //   if (bod[0]) {
+    //     bodyTxt = bod[0].nextSibling.textContent;
+    //   }
+    // }
+
+    // setBodyTxt(bodyTxt);
+    const galleryComponent = _buildGallery();
+    // if (galleryComponent) {
+        setGalleryComponent(galleryComponent);
+    // }
+  }, []);
+
+  return (
+    <div className="bg-white pb-4 mb-6">
+          {
+              galleryComponent ? galleryComponent :null
+          }  
+    </div>
+  )
+}
