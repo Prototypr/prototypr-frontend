cd903e9d3784e5f57cf1fbdfc9397fe263338a09
wendyjisung <wendyjisung@aliyun.com>
2022-03-24 22:51:00 +0800

    feat: Topic Spolights list

--- components/new-index/TopicTopItem.js
+++ components/new-index/TopicTopItem.js
@@ -1,5 +1,5 @@
 
-
+import Author from './Author'
 export default function TopicTopItem({}) {
 
     return (
@@ -13,8 +13,9 @@ export default function TopicTopItem({}) {
                 <h4 className="text-black-1 font-bold text-lg leading-normal mt-2">Designing with data — 4 principles and a cheat sheet</h4>
                 <p className="mt-3 text-gray-3 font-medium text-base leading-normal overflow-hidden text-ellipsis clamp-2">Guiding leadership values for 2022. Excellent leadership bla bla for testing ellipse</p>
                 <div className="flex items-center mt-5">
-                    <div style={{width: "36px",height: "36px",border: "1px solid red"}} className="rounded-full mr-3"></div>
-                    <div className="font-medium text-base leading-normal text-gray-1">Justin Rhiel Madsen</div>
+                    <Author />
+                    {/* <div style={{width: "36px",height: "36px",border: "1px solid red"}} className="rounded-full mr-3"></div>
+                    <div className="font-medium text-base leading-normal text-gray-1">Justin Rhiel Madsen</div> */}
                 </div>
             </div>
         </div>
