a695a6608c8da14436eddfe3e0ad8ee32d65dc4d
wendyjisung <wendyjisung@aliyun.com>
2022-03-26 22:51:32 +0800

    fix: tailwind.config error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     fontFamily: {
-      "inter": "Inter"
+      inter: "Inter",
     },
     extend: {
       gridTemplateColumns: {
         // Simple 16 column grid
-        '24': 'repeat(24, minmax(0, 1fr))'
+        24: 'repeat(24, minmax(0, 1fr))'
       },
       gridColumn: {
-        'span-13': 'span 13 / span 13',
-        'span-14': 'span 14 / span 14',
-        'span-15': 'span 15 / span 15',
-        'span-16': 'span 16 / span 16',
-        'span-17': 'span 17 / span 17',
-        'span-18': 'span 18 / span 18',
-        'span-19': 'span 19 / span 19',
-        'span-20': 'span 20 / span 20',
-        'span-21': 'span 21 / span 21',
-        'span-22': 'span 22 / span 22',
-        'span-23': 'span 23 / span 23',
-        'span-24': 'span 24 / span 24',
+        "span-13": "span 13 / span 13",
+        "span-14": "span 14 / span 14",
+        "span-15": "span 15 / span 15",
+        "span-16": "span 16 / span 16",
+        "span-17": "span 17 / span 17",
+        "span-18": "span 18 / span 18",
+        "span-19": "span 19 / span 19",
+        "span-20": "span 20 / span 20",
+        "span-21": "span 21 / span 21",
+        "span-22": "span 22 / span 22",
+        "span-23": "span 23 / span 23",
+        "span-24": "span 24 / span 24",
       },
       colors: {
         primary: {
@@ -40,19 +40,19 @@ module.exports = {
           800: "#13204A",
           900: "#080D1E",
         },
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        'title-1': '#2B2B2B',
-        'paragraph-1': '#010301',
-        'gray-1': '#333333',
-        'gray-2': '#4F4F4F',
-        'gray-3': '#828282',
-        'gray-4': '#F3F4F6',
-        'gray-5': '#E0E0E0',
-        'blue-1': '#2463EB',
-        'neutrals-700':'#4A5568',
-        'black-1': '#222222',
+        "accent-1": "#FAFAFA",
+        "accent-2": "#EAEAEA",
+        "accent-7": "#333",
+        "title-1": "#2B2B2B",
+        "paragraph-1": "#010301",
+        "gray-1": "#333333",
+        "gray-2": "#4F4F4F",
+        "gray-3": "#828282",
+        "gray-4": "#F3F4F6",
+        "gray-5": "#E0E0E0",
+        "blue-1": "#2463EB",
+        "neutrals-700":"#4A5568",
+        "black-1": "#222222",
         success: '#0070f3',
         cyan: '#79FFE1',
       },
@@ -66,21 +66,30 @@ module.exports = {
         65: '17rem'
       },
       letterSpacing: {
-        tighter: '-.04em',
+        tighter: "-.04em",
       },
       lineHeight: {
         tight: 1.2,
       },
       fontSize: {
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
-        's6xl': '3.43rem'
+        "5xl": "2.5rem",
+        "6xl": "2.75rem",
+        "7xl": "4.5rem",
+        "8xl": "6.25rem",
+        "s6xl": "3.43rem"
       },
       boxShadow: {
-        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
+        small: "0 5px 10px rgba(0, 0, 0, 0.12)",
+        medium: "0 8px 30px rgba(0, 0, 0, 0.12)",
+      },
+      animation: {
+        spinner: "spinner 1.5s linear infinite",
+      },
+      keyframes: {
+        spinner: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
       },
     },
   },
