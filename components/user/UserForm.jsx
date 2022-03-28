58bd76ef1edf712a78596b16e4c801635fea605d
Tergel Munkhdelger <tergelm@gmail.com>
2022-03-28 16:38:23 +0800

    user form data binding

--- components/user/UserForm.jsx
+++ components/user/UserForm.jsx
@@ -1,247 +1,251 @@
 import FormControl from "@/components/atom/FormControl/FormControl";
 import { accountLocations } from "@/lib/constants";
+import axios from "axios";
 import { useSession } from "next-auth/react";
+import qs from "query-string";
 import { useForm } from "react-hook-form";
 import Button from "../atom/Button/Button";
-import Fallback from "../atom/Fallback/Fallback";
 
-// demo
-function timeout(delay) {
-  return new Promise((res) => setTimeout(res, delay));
-}
-
-const FormContainer = () => {
-  const { data: sessionInfo, status } = useSession();
-  console.log(sessionInfo);
-
-  if (status === "loading") {
-    return <Fallback />;
-  }
-
-  if (status === "authenticated") {
-    return (
-      <UserForm
-        info={{
-          email: sessionInfo.user.email,
-          username: sessionInfo.user.name,
-        }}
-      />
-    );
-  }
-
-  return <div>Unauthenticated whoops</div>;
-};
 const UserForm = ({ info }) => {
+  const {
+    data: { jwt },
+  } = useSession();
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting, errors },
   } = useForm({
     defaultValues: {
-      location: "",
-      firstName: "",
-      secondName: "",
-      website: "",
-      bio: "",
-      paymentPointer: "",
+      location: info.location,
+      firstName: info.firstName,
+      secondName: info.secondName,
+      website: info.website,
+      bio: info.bio,
+      paymentPointer: info.paymentPointer,
       email: info.email,
       username: info.username,
     },
   });
 
   const onSubmit = async (data) => {
-    console.log(data);
-    await timeout(5000);
+    if (!data.location) {
+      // if location not provided
+      // clear it
+      data.location = undefined;
+    }
+
+    try {
+      await axios({
+        method: "POST",
+        url:
+          process.env.NEXT_PUBLIC_API_URL + "/api/users-permissions/users/me",
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: qs.stringify(data),
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl inValid={!!errors.firstName}>
-        <label htmlFor="firstName" className="text-sm">
-          First name
-        </label>
-        <input
-          id="firstName"
-          type="text"
-          autoComplete="off"
-          className="w-full"
-          placeholder="John"
-          {...register("firstName", {
-            disabled: isSubmitting,
-            maxLength: {
-              message: "Maximum length can be up to 50 characters",
-              value: 50,
-            },
-          })}
-        />
-        {errors.firstName && (
-          <span className="error">{errors.firstName.message}</span>
-        )}
-      </FormControl>
-      <FormControl inValid={!!errors.secondName}>
-        <label htmlFor="secondName" className="text-sm">
-          Last name
-        </label>
-        <input
-          id="secondName"
-          type="text"
-          autoComplete="off"
-          className="w-full"
-          placeholder="Doe"
-          {...register("secondName", {
-            disabled: isSubmitting,
-            maxLength: {
-              message: "Maximum length can be up to 50 characters",
-              value: 50,
-            },
-          })}
-        />
-        {errors.secondName && (
-          <span className="error">{errors.secondName.message}</span>
-        )}
-      </FormControl>
-      <FormControl inValid={!!errors.location}>
-        <label htmlFor="location" className="text-sm">
-          Location
-        </label>
-        <select
-          id="location"
-          className="w-full"
-          {...register("location", {
-            disabled: isSubmitting,
-          })}
-        >
-          {accountLocations.map((i) => (
-            <option key={i} value={i}>
-              {i}
-            </option>
-          ))}
-        </select>
-        {errors.location && (
-          <span className="error">{errors.location.message}</span>
-        )}
-      </FormControl>
-      <FormControl inValid={!!errors.website}>
-        <label htmlFor="website" className="text-sm">
-          Personal Website
-        </label>
-        <input
-          id="website"
-          type="text"
-          autoComplete="off"
-          className="w-full"
-          placeholder="https://myprofile.com"
-          {...register("website", {
-            disabled: isSubmitting,
-            maxLength: {
-              message: "Maximum length can be up to 50 characters",
-              value: 120,
-            },
-          })}
-        />
-        {errors.website && (
-          <span className="error">{errors.website.message}</span>
-        )}
-      </FormControl>
-      <FormControl inValid={!!errors.bio}>
-        <label htmlFor="bio" className="text-sm">
-          Bio
-        </label>
-        <textarea
-          id="bio"
-          type="text"
-          autoComplete="off"
-          className="w-full resize-y h-auto"
-          rows={4}
-          placeholder="Where are you from? What's your role? What's your favourite animal?"
-          {...register("bio", {
-            disabled: isSubmitting,
-            maxLength: {
-              message: "Maximum length can be up to 50 characters",
-              value: 160,
-            },
-          })}
-        />
-        {errors.bio && <span className="error">{errors.bio.message}</span>}
-      </FormControl>
-      <FormControl inValid={!!errors.paymentPointer}>
-        <label htmlFor="paymentPointer" className="text-sm">
-          Payment Pointer
-        </label>
-        <input
-          id="paymentPointer"
-          type="text"
-          autoComplete="off"
-          className="w-full"
-          placeholder="$alice.wallet.example"
-          {...register("paymentPointer", {
-            disabled: isSubmitting,
-            maxLength: {
-              message: "Maximum length can be up to 50 characters",
-              value: 120,
-            },
-          })}
-        />
-        {errors.paymentPointer && (
-          <span className="error">{errors.paymentPointer.message}</span>
-        )}
-      </FormControl>
+      <div className="flex flex-col gap-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+          <FormControl inValid={!!errors.firstName}>
+            <label htmlFor="firstName" className="text-sm">
+              First name
+            </label>
+            <input
+              id="firstName"
+              type="text"
+              autoComplete="off"
+              className="w-full"
+              placeholder="John"
+              {...register("firstName", {
+                disabled: isSubmitting,
+                maxLength: {
+                  message: "Maximum length can be up to 50 characters",
+                  value: 50,
+                },
+              })}
+            />
+            {errors.firstName && (
+              <span className="error">{errors.firstName.message}</span>
+            )}
+          </FormControl>
+          <FormControl inValid={!!errors.secondName}>
+            <label htmlFor="secondName" className="text-sm">
+              Last name
+            </label>
+            <input
+              id="secondName"
+              type="text"
+              autoComplete="off"
+              className="w-full"
+              placeholder="Doe"
+              {...register("secondName", {
+                disabled: isSubmitting,
+                maxLength: {
+                  message: "Maximum length can be up to 50 characters",
+                  value: 50,
+                },
+              })}
+            />
+            {errors.secondName && (
+              <span className="error">{errors.secondName.message}</span>
+            )}
+          </FormControl>
+          <FormControl inValid={!!errors.location}>
+            <label htmlFor="location" className="text-sm">
+              Location
+            </label>
+            <select
+              id="location"
+              className="w-full"
+              {...register("location", {
+                disabled: isSubmitting,
+              })}
+            >
+              {accountLocations.map((i) => (
+                <option key={i} value={i}>
+                  {i}
+                </option>
+              ))}
+            </select>
+            {errors.location && (
+              <span className="error">{errors.location.message}</span>
+            )}
+          </FormControl>
+          <FormControl inValid={!!errors.website}>
+            <label htmlFor="website" className="text-sm">
+              Personal Website
+            </label>
+            <input
+              id="website"
+              type="text"
+              autoComplete="off"
+              className="w-full"
+              placeholder="https://myprofile.com"
+              {...register("website", {
+                disabled: isSubmitting,
+                maxLength: {
+                  message: "Maximum length can be up to 50 characters",
+                  value: 120,
+                },
+              })}
+            />
+            {errors.website && (
+              <span className="error">{errors.website.message}</span>
+            )}
+          </FormControl>
+        </div>
 
-      <FormControl inValid={!!errors.email}>
-        <label htmlFor="email" className="text-sm">
-          Email
-        </label>
-        <input
-          id="email"
-          type="email"
-          autoComplete="off"
-          className="w-full"
-          placeholder="john@mail.com"
-          {...register("email", {
-            // disabled: isSubmitting,
-            disabled: true,
-            required: true,
-            maxLength: {
-              message: "Maximum length can be up to 120 characters",
-              value: 120,
-            },
-          })}
-        />
-        {errors.email && <span className="error">{errors.email.message}</span>}
-      </FormControl>
+        <FormControl inValid={!!errors.bio}>
+          <label htmlFor="bio" className="text-sm">
+            Bio
+          </label>
+          <textarea
+            id="bio"
+            type="text"
+            autoComplete="off"
+            className="w-full resize-y h-auto"
+            rows={4}
+            placeholder="Where are you from? What's your role? What's your favourite animal?"
+            {...register("bio", {
+              disabled: isSubmitting,
+              maxLength: {
+                message: "Maximum length can be up to 50 characters",
+                value: 160,
+              },
+            })}
+          />
+          {errors.bio && <span className="error">{errors.bio.message}</span>}
+        </FormControl>
+        <FormControl inValid={!!errors.paymentPointer}>
+          <label htmlFor="paymentPointer" className="text-sm">
+            Payment Pointer
+          </label>
+          <input
+            id="paymentPointer"
+            type="text"
+            autoComplete="off"
+            className="w-full"
+            placeholder="$alice.wallet.example"
+            {...register("paymentPointer", {
+              disabled: isSubmitting,
+              maxLength: {
+                message: "Maximum length can be up to 50 characters",
+                value: 120,
+              },
+            })}
+          />
+          {errors.paymentPointer && (
+            <span className="error">{errors.paymentPointer.message}</span>
+          )}
+        </FormControl>
 
-      <FormControl inValid={!!errors.username}>
-        <label htmlFor="username" className="text-sm">
-          Username
-        </label>
-        <input
-          id="username"
-          type="text"
-          autoComplete="off"
-          className="w-full"
-          placeholder="John"
-          {...register("username", {
-            disabled: true,
-            // disabled: isSubmitting,
-            required: true,
-            maxLength: {
-              message: "Maximum length can be up to 120 characters",
-              value: 120,
-            },
-          })}
-        />
-        {errors.username && (
-          <span className="error">{errors.username.message}</span>
-        )}
-      </FormControl>
+        <FormControl inValid={!!errors.email}>
+          <label htmlFor="email" className="text-sm">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            autoComplete="off"
+            className="w-full"
+            placeholder="john@mail.com"
+            {...register("email", {
+              // disabled: isSubmitting,
+              disabled: true,
+              required: true,
+              maxLength: {
+                message: "Maximum length can be up to 120 characters",
+                value: 120,
+              },
+            })}
+          />
+          {errors.email && (
+            <span className="error">{errors.email.message}</span>
+          )}
+        </FormControl>
 
-      <div className="mt-6">
-        <Button isLoading={isSubmitting} type="submit" color="primary">
-          Save Profile Info
-        </Button>
+        <FormControl inValid={!!errors.username}>
+          <label htmlFor="username" className="text-sm">
+            Username
+          </label>
+          <input
+            id="username"
+            type="text"
+            autoComplete="off"
+            className="w-full"
+            placeholder="John"
+            {...register("username", {
+              disabled: true,
+              // disabled: isSubmitting,
+              required: true,
+              maxLength: {
+                message: "Maximum length can be up to 120 characters",
+                value: 120,
+              },
+            })}
+          />
+          {errors.username && (
+            <span className="error">{errors.username.message}</span>
+          )}
+        </FormControl>
+
+        <div className="mt-6">
+          <Button isLoading={isSubmitting} type="submit" color="primary">
+            Save Profile Info
+          </Button>
+        </div>
       </div>
     </form>
   );
 };
 
-export default FormContainer;
+export default UserForm;
--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -1,37 +1,88 @@
+import Fallback from "@/components/atom/Fallback/Fallback";
 import Container from "@/components/container";
 import Layout from "@/components/layout";
 import UserForm from "@/components/user/UserForm";
+import axios from "axios";
+import { getToken } from "next-auth/jwt";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 
-const AccountPage = ({ preview }) => {
-  return (
-    <Layout preview={preview}>
-      <Head>
-        <title>Account Settings</title>
-      </Head>
-      <Container>
-        <div className="bg-white shadow-md rounded-lg py-6 px-4">
-          <h1 className="font-semibold">Public Profile</h1>
-          <span className="text-sm text-gray-400">
-            This information will be displayed publicly on your profile
-          </span>
-          <UserForm />
-        </div>
-      </Container>
-    </Layout>
-  );
+const AccountPage = ({ preview, userData }) => {
+  const { data: sessionInfo, status } = useSession();
+  if (status === "loading") {
+    return <Fallback />;
+  }
+
+  if (status === "authenticated") {
+    return (
+      <Layout preview={preview}>
+        <Head>
+          <title>Account Settings</title>
+        </Head>
+        <Container>
+          <div className="pt-6 pb-10 md:pt-10">
+            <div className="bg-white shadow-md rounded-lg py-6 px-4">
+              <h1 className="font-semibold">Public Profile</h1>
+              <span className="text-sm text-gray-400">
+                This information will be displayed publicly on your profile
+              </span>
+              <UserForm
+                info={{
+                  firstName: userData.firstName,
+                  secondName: userData.secondName,
+                  location: userData.location,
+                  website: userData.website,
+                  bio: userData.bio,
+                  paymentPointer: userData.paymentPointer,
+
+                  // ask about these later
+                  email: sessionInfo.user.email,
+                  username: sessionInfo.user.name,
+                }}
+              />
+            </div>
+          </div>
+        </Container>
+      </Layout>
+    );
+  }
+
+  // TODO
+  return <div>Unauthenticated whoops</div>;
 };
 
-// export async function getServerSideProps(context) {
-//   const session = await getSession({
-//     req: context.req,
-//   });
+export async function getServerSideProps(context) {
+  try {
+    const token = await getToken({
+      req: context.req,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+    if (token) {
+      const res = await axios({
+        method: "GET", // change this GET later
+        url: process.env.NEXT_PUBLIC_API_URL + "/api/users/me",
+        headers: {
+          Authorization: `Bearer ${token.jwt}`,
+        },
+      });
 
-//   const userData = await getUserInfo(session.id);
-//   console.log(userData);
-//   return {
-//     props: {}, // will be passed to the page component as props
-//   };
-// }
+      return {
+        props: {
+          userData: res.data,
+        }, // will be passed to the page component as props
+      };
+    }
+    // TODO redirect back to home
+    return {
+      notFound: true,
+    };
+  } catch (error) {
+    console.log(error);
+    // TODO 500 server error
+    return {
+      notFound: true,
+    };
+  }
+}
 
 export default AccountPage;
