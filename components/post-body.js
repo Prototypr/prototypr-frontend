00b4745ad9a21d2fe256c87973696264a3a98e45
Graeme Fulton <graemefulton89@gmail.com>
2022-04-01 03:07:54 +0200

    opt:post page styles (copy from old site)

--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,10 @@
-import markdownStyles from './markdown-styles.module.css'
+// import markdownStyles from './markdown-styles.module.css'
 
 export default function PostBody({ content }) {
   return (
     <div className="max-w-2xl mx-auto blog-content">
       <div
-        className={markdownStyles['markdown']}
+        // className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
