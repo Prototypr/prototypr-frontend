34da0f9734dfaaad2a55f38cfb2da863f074d587
Graeme Fulton <graeme@prototypr.io>
2022-04-03 11:28:12 +0000

    Update ProductItem.js and index.scss

--- components/new-index/ProductItem.js
+++ components/new-index/ProductItem.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export default function ProductItem({ post = {} }) {
   const {
     title = "",
@@ -11,10 +12,14 @@ export default function ProductItem({ post = {} }) {
   const tagArr = tags.data;
   return (
     <div className="grid-cols-1 flex items-center cursor-pointer">
-      <div
-        className="w-40 h-40 rounded-lg bg-contain bg-no-repeat"
-        style={{ backgroundImage: `url(${legacyFeaturedImage?.thumb})` }}
-      ></div>
+      <div className="w-40 h-40 rounded-lg bg-contain bg-no-repeat relative">
+        <Image
+          objectFit="cover"
+          className="rounded-lg"
+          layout="fill"
+          src={legacyFeaturedImage?.thumb}
+        />
+      </div>
       <div className="flex-1 ml-4 h-full">
         <div className="flex">
           <div className="font-light text-sm leading-6 tracking-wide uppercase text-gray-3 mr-1">
@@ -28,12 +33,14 @@ export default function ProductItem({ post = {} }) {
           {title}
         </h4>
         <div className="mt-3 flex items-center">
-          <div
-            className="w-9 h-9 rounded-full bg-100 bg-no-repeat bg-center"
-            style={{
-              backgroundImage: `url(${author?.data?.attributes?.avatar})`,
-            }}
-          ></div>
+          <div className="w-9 h-9 rounded-full relative">
+            <Image
+              src={author?.data?.attributes?.avatar}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-full"
+            />
+          </div>
           <div className="font-medium text-base ml-3">
             {author?.data?.attributes?.displayName}
           </div>
