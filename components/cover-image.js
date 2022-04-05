bf5d45fa11c8bd3bcdd4e3aa55f4713c70aef850
Graeme Fulton <graeme@prototypr.io>
2022-04-05 23:38:46 +0000

    opt: add hover effect and link to toolbox

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,38 +1,39 @@
-import cn from 'classnames'
-import Image from 'next/image'
-import Link from 'next/link'
+import cn from "classnames";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function CoverImage({ title, url, slug, type, route }) {
+  url = url?.mediaItemUrl;
 
-  url = url?.mediaItemUrl
+  const imageUrl = url
+    ? `${
+        url.startsWith("/") ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ""
+      }${url}`
+    : null;
 
-  const imageUrl = url?`${
-    url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''
-  }${url}`:null
-
-  const image = (
-    url?<Image
-      width={2000}
-      height={type=='toolbox'?1500:1200}
-      // layout='fill'
-      objectFit="cover"
-      alt={`Cover Image for ${title}`}
-      src={imageUrl}
-      className="rounded-lg"
-      // className={cn('shadow-small', {
-      //   'hover:shadow-medium transition-shadow duration-200': slug,
-      // })}
-    />:null
-  )
+  const image = url ? (
+    <figure className="relative w-full h-60 md:h-40 overflow-hidden rounded-lg transform group-hover:translate-x-0 group-hover:shadow group-hover:translate-y-0 transition duration-700 ease-out overflow-hidden">
+      <div className="absolute  w-full h-full object-cover rounded-lg transform group-hover:scale-105 transition duration-700 ease-out cursor-pointer">
+        <Link href={`/${type ? type : route ? route : "posts"}/${slug}`}>
+          <Image
+            className="rounded-lg contrast-115"
+            objectFit="cover"
+            layout="fill"
+            src={imageUrl}
+          />
+        </Link>
+      </div>
+    </figure>
+  ) : null;
   return (
     <div className="sm:mx-0">
       {slug ? (
-        <Link href={`/${type?type:route?route:'posts'}/${slug}`}>
+        <Link href={`/${type ? type : route ? route : "posts"}/${slug}`}>
           <a aria-label={title}>{image}</a>
         </Link>
       ) : (
         image
       )}
     </div>
-  )
+  );
 }
