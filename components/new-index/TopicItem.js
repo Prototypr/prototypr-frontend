53c5ad9c8fbdc2ddd91a802296a353659481b46a
Graeme Fulton <graeme@prototypr.io>
2022-04-19 08:44:40 +0000

    fix: responsive issues on homepage for safari mobile

--- components/new-index/TopicItem.js
+++ components/new-index/TopicItem.js
@@ -15,7 +15,7 @@ export default function TopicItem({ topic = {} }) {
 
   return (
     <div className="grid-cols-1 p-3 flex cursor-pointer group">
-      <figure className="relative h-0 w-40 h-40 overflow-hidden rounded-lg transform group-hover:translate-x-0 group-hover:shadow group-hover:translate-y-0 transition duration-700 ease-out overflow-hidden">
+      <figure className="relative h-0 mr-1 sm:mr-0 w-28 h-28 sm:w-40 sm:h-40 overflow-hidden rounded-lg transform group-hover:translate-x-0 group-hover:shadow group-hover:translate-y-0 transition duration-700 ease-out overflow-hidden">
         <div className="absolute  w-full h-full object-cover rounded-lg transform group-hover:scale-105 transition duration-700 ease-out cursor-pointer">
           <Link href={`/posts/${slug}`}>
             <Image
@@ -29,7 +29,7 @@ export default function TopicItem({ topic = {} }) {
       </figure>
       <div className="flex flex-col flex-1 ml-4">
         <div className="flex">
-          <div className="font-normal text-sm leading-6 tracking-wide uppercase text-gray-3 mr-2">
+          <div className="font-normal text-xs sm:text-sm leading-6 tracking-wide uppercase text-gray-3 mr-2">
             # {tagArr && tagArr.length ? tagArr[0].attributes.slug : "design"}
           </div>
           {/* <div className="font-medium text-sm leading-6 tracking-wide uppercase text-gray-3 mr-2"># product design</div> */}
