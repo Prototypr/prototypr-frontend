18d24cb416c9439999ce0eafad8ec240ff9411a4
Carmen Qiu <carmenqiu1990@gmail.com>
2022-04-19 15:19:41 +0000

    fix:show 8 IssueItem at a time

--- components/newsletter/IssueList.js
+++ components/newsletter/IssueList.js
@@ -12,6 +12,10 @@ export default function IssueList({ posts = [] }) {
         <IssueItem />
         <IssueItem />
         <IssueItem />
+        <IssueItem />
+        <IssueItem />
+        <IssueItem />
+        <IssueItem />
       </div>
       <div className="flex items-center justify-center">
         <button className="bg-white rounded-lg px-18 py-4 font-semibold text-base flex items-center justify-center text-gray-1 hover:opacity-80">View all</button>
