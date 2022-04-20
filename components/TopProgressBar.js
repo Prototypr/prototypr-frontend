bd2685a8f724f6308caa4616555b941f8309d2c3
Graeme Fulton <graemefulton89@gmail.com>
2022-04-20 11:33:36 +0200

    opt: remove window fetch bit from progress bar

--- components/TopProgressBar.js
+++ components/TopProgressBar.js
@@ -33,26 +33,26 @@ Router.events.on("routeChangeStart", load);
 Router.events.on("routeChangeComplete", stop);
 Router.events.on("routeChangeError", stop);
 
-const originalFetch = window.fetch;
-window.fetch = async function (...args) {
-  if (activeRequests === 0) {
-    load();
-  }
+// const originalFetch = window.fetch;
+// window.fetch = async function (...args) {
+//   if (activeRequests === 0) {
+//     load();
+//   }
 
-  activeRequests++;
+//   activeRequests++;
 
-  try {
-    const response = await originalFetch(...args);
-    return response;
-  } catch (error) {
-    return Promise.reject(error);
-  } finally {
-    activeRequests -= 1;
-    if (activeRequests === 0) {
-      stop();
-    }
-  }
-};
+//   try {
+//     const response = await originalFetch(...args);
+//     return response;
+//   } catch (error) {
+//     return Promise.reject(error);
+//   } finally {
+//     activeRequests -= 1;
+//     if (activeRequests === 0) {
+//       stop();
+//     }
+//   }
+// };
 
 export default function () {
   return null;
