fd4dc41bdf835f935d4d1a258b34f49f07dd27e9
Carmen Qiu <carmenqiu1990@gmail.com>
2022-04-26 01:19:16 +0000

    feat:#88 the new people page query

--- pages/people/page/[pageNo].js
+++ pages/people/page/[pageNo].js
@@ -6,4 +6,37 @@ const MoreStories = dynamic(() => import("@/components/more-stories"));
 const NewPagination = dynamic(() => import("@/components/pagination"));
 const FilterCategory = dynamic(() => import("@/components/FilterCategory"));
 const Breadcrumbs = dynamic(() => import("@/components/Breadcrumbs"));
+const PAGE_SIZE = 12;
+import { getPeopleByPage } from "@/lib/api";
 
+export default function PeoplePage({
+
+}) {
+    return (
+        <div>1111</div>
+    )
+}
+
+export async function getStaticProps({ preview = null, params,locale }) {
+    return {
+        props: {
+            
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const allPosts = (await getPeopleByPage(null, PAGE_SIZE, 0)) || [];
+    const pagination = allPosts.meta.pagination;
+    const pageCount = pagination.pageCount;
+    const pageCountArr = new Array(pageCount).fill(" ");
+    return {
+        paths:
+          (pageCountArr &&
+            pageCountArr.map((pageNo, index) => {
+              return `/people/page/${index}`;
+            })) ||
+          [],
+        fallback: true,
+      };
+}
