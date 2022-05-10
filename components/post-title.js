1598e45cde0f5c8253e5765e6a93ec2db302049b
Graeme Fulton <graeme@prototypr.io>
2022-05-10 11:24:50 +0000

    Update post-title.js and posts-page.scss

--- components/post-title.js
+++ components/post-title.js
@@ -1,7 +1,7 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="text-6xl font-semibold tracking-tighter leading-tight md:leading-tighter mb-5 text-center md:text-left">
+    <h1 className="text-6xl font-noto-serif font-semibold tracking-tighter leading-tight md:leading-tighter mb-5 text-center md:text-left">
       {children}
     </h1>
-  )
+  );
 }
