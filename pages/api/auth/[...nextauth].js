672790f5243c1def5eb474305265e892ad54c867
Graeme Fulton <graemefulton89@gmail.com>
2022-05-20 12:56:05 +0200

    fix:catch errors with login - strapi only allows login with 1 provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ const options = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      idToken: false
     }),
     TwitterProvider({
       clientId: process.env.TWITTER_ID,
@@ -30,7 +31,11 @@ const options = {
           if(account.provider=='twitter'){
             url.searchParams.set("access_token", account.oauth_token)
             url.searchParams.set("access_secret", account.oauth_token_secret) // sending the refreshToken as access_secret
-          }else{
+          }else if(account.provider=='github'){
+            url.searchParams.set("access_token", account.access_token)
+            url.searchParams.set("access_secret", account.refreshToken) // sending the refreshToken as access_secret
+          }
+          else{
             if(account && account.provider){
               // https://gitmemory.com/issue/strapi/strapi/9492/784032615
               url.searchParams.set("access_token", account.access_token)
@@ -42,6 +47,15 @@ const options = {
           url.toString(), 
         );
         const data = await response.json();
+        /**
+         * a given email address can only sign in with 1 provider
+         * if they already registered with twitter, and try to sign in with GitHub
+         * strapi will give an error - so redirect them to the nextauth unauthorized
+         * page for now. Not pretty but it works until a custom error page is made
+         */
+        if(data?.error){
+          return '/unauthorized'
+        }
 
         token.jwt = data.jwt;
         token.id = user.id;
