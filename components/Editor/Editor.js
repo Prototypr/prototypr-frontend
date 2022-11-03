30afbb2fbf7853a45ab316f5705d2afc431e34ee
Joel Benjamin <joelbenjamin093@gmail.com>
2022-11-03 23:15:30 +0530

    admin restrictions

--- components/Editor/Editor.js
+++ components/Editor/Editor.js
@@ -1,11 +1,11 @@
 import { useEditor, EditorContent } from "@tiptap/react";
-import MenuFloating from './Menus/FloatingMenu'
+import MenuFloating from "./Menus/FloatingMenu";
 import EditorNav from "../EditorNav";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
 import Placeholder from "@tiptap/extension-placeholder";
 import Document from "@tiptap/extension-document";
-import TextMenu from '@/components/Editor/Menus/TextMenu'
+import TextMenu from "@/components/Editor/Menus/TextMenu";
 import Button from "../Primitives/Button";
 
 import Link from "@tiptap/extension-link";
@@ -20,48 +20,60 @@ import Image from "@tiptap/extension-image";
 import Gapcursor from "@tiptap/extension-gapcursor";
 import Youtube from "@tiptap/extension-youtube";
 import HorizontalRule from "@tiptap/extension-horizontal-rule";
-import Text from '@tiptap/extension-text'
-import Heading from '@tiptap/extension-heading'
-import Paragraph from '@tiptap/extension-paragraph'
+import Text from "@tiptap/extension-text";
+import Heading from "@tiptap/extension-heading";
+import Paragraph from "@tiptap/extension-paragraph";
 import CodeBlock from "@tiptap/extension-code-block";
-import Bold from '@tiptap/extension-bold'
-import HardBreak from '@tiptap/extension-hard-break'
-import Underline from '@tiptap/extension-underline'
-import Italic from '@tiptap/extension-italic'
-import ListItem from '@tiptap/extension-list-item'
-import BulletList from '@tiptap/extension-bullet-list'
-import OrderedList from '@tiptap/extension-ordered-list'
-import Dropcursor from '@tiptap/extension-dropcursor'
+import Bold from "@tiptap/extension-bold";
+import HardBreak from "@tiptap/extension-hard-break";
+import Underline from "@tiptap/extension-underline";
+import Italic from "@tiptap/extension-italic";
+import ListItem from "@tiptap/extension-list-item";
+import BulletList from "@tiptap/extension-bullet-list";
+import OrderedList from "@tiptap/extension-ordered-list";
+import Dropcursor from "@tiptap/extension-dropcursor";
 import History from "@tiptap/extension-history";
-import {Blockquote} from "@/components/Editor/CustomExtensions/CustomBlockquote";
-import Figure from "./CustomExtensions/Figure"
-import Tweet from "./CustomExtensions/Tweet/Tweet"
-import Video from "./CustomExtensions/Video/Video"
+import { Blockquote } from "@/components/Editor/CustomExtensions/CustomBlockquote";
+import Figure from "./CustomExtensions/Figure";
+import Tweet from "./CustomExtensions/Tweet/Tweet";
+import Video from "./CustomExtensions/Video/Video";
 
-import {useConfirmTabClose} from './useConfirmTabClose'
-import {useCreate, useLoad, useUpdate} from './editorHooks/index'
+import { useConfirmTabClose } from "./useConfirmTabClose";
+import { useCreate, useLoad, useUpdate } from "./editorHooks/index";
 
 import { useRouter } from "next/router";
-const Spinner = dynamic(() => import('@/components/atom/Spinner/Spinner'))
+const Spinner = dynamic(() => import("@/components/atom/Spinner/Spinner"));
 
 const CustomDocument = Document.extend({
   content: "heading block*",
   atom: true,
 });
 
-
-const Editor = ({ editorType = "create" }) => {
+const Editor = ({
+  editorType = "create",
+  hasEditPermission = true,
+  mode = "user",
+}) => {
   const router = useRouter();
 
   const { user } = useUser({
     redirectIfFound: false,
   });
 
-  const {content, loading, slug,title,postId, postStatus} = useLoad(editorType, user)
-  const {updateExisitingPost, setSaving, setHasUnsavedChanges, hasUnsavedChanges, saving} = useUpdate()
-  const {createNewPost} = useCreate()
+  const { content, loading, slug, title, postId, postStatus } = useLoad(
+    editorType,
+    user
+  );
+  const {
+    updateExisitingPost,
+    setSaving,
+    setHasUnsavedChanges,
+    hasUnsavedChanges,
+    saving,
+  } = useUpdate();
+  const { createNewPost } = useCreate();
 
-  const [editorCreated, setEditorCreated] = useState(false) 
+  const [editorCreated, setEditorCreated] = useState(false);
 
   useConfirmTabClose(hasUnsavedChanges);
 
@@ -102,20 +114,20 @@ const Editor = ({ editorType = "create" }) => {
           if (node.type.name === "heading") {
             return "What's the title?";
           }
-            return "Tell a story...";
+          return "Tell a story...";
         },
       }),
     ],
-    onCreate:()=>{
-      setTimeout(()=>{
-        setEditorCreated(true)
-      },1200)
+    onCreate: () => {
+      setTimeout(() => {
+        setEditorCreated(true);
+      }, 1200);
     },
     onUpdate: ({ editor }) => {
       const json = editor.getJSON();
       setHasUnsavedChanges(true);
       // send the content to an API here (if new post only)
-      if(!slug){
+      if (!slug) {
         localStorage.setItem("wipContent", JSON.stringify(json));
       }
     },
@@ -125,13 +137,24 @@ const Editor = ({ editorType = "create" }) => {
   useEffect(() => {
     if (editor && !editor.isDestroyed) {
       if (editor?.commands) {
-          editor?.commands?.setContent(content);
-        }
+        editor?.commands?.setContent(content);
       }
+    }
   }, [content]);
 
-
-
+  useEffect(() => {
+    if (editor) {
+      if (mode === "admin") {
+        if (!hasEditPermission) {
+          console.log("cant edit");
+          editor.setEditable(false);
+        } else {
+          console.log("can edit");
+          editor.setEditable(true);
+        }
+      }
+    }
+  }, [editor, hasEditPermission, mode]);
 
   const onSubmit = async () => {
     // before submitting, check strapi if slug or id already exists
@@ -140,11 +163,11 @@ const Editor = ({ editorType = "create" }) => {
       const postInfo = await createNewPost(user, editor);
       //set the new slug
       localStorage.removeItem("wipContent");
-      router.push(`my-posts/draft/${postInfo?.attributes?.slug}`)
+      router.push(`my-posts/draft/${postInfo?.attributes?.slug}`);
     }
 
     if (slug) {
-      await updateExisitingPost(postId,user, editor, slug, true, postStatus);
+      await updateExisitingPost(postId, user, editor, slug, true, postStatus);
     }
   };
 
@@ -154,17 +177,24 @@ const Editor = ({ editorType = "create" }) => {
       setSaving(true);
       try {
         console.log("saving...");
-        console.log(postId)
-        await updateExisitingPost(postId,user, editor, slug, false, postStatus);
+        console.log(postId);
+        await updateExisitingPost(
+          postId,
+          user,
+          editor,
+          slug,
+          false,
+          postStatus
+        );
       } catch (e) {
         setSaving(false);
       }
-    }else{
+    } else {
       if (editorType === "create") {
         const postInfo = await createNewPost(user, editor);
         //set the new slug
         localStorage.removeItem("wipContent");
-        router.push(`my-posts/draft/${postInfo?.attributes?.slug}`)
+        router.push(`my-posts/draft/${postInfo?.attributes?.slug}`);
       }
     }
   };
@@ -190,89 +220,119 @@ const Editor = ({ editorType = "create" }) => {
 
   return (
     <div className="w-full relative my-4">
-     {/* NAVIGATION, WITH BUTTONS EMBEDDED AS A PROP */}
-      <EditorNav 
-      postStatus={postStatus}
-      isEditor={true}
-      editorButtons = {
+      {/* NAVIGATION, WITH BUTTONS EMBEDDED AS A PROP */}
+      {mode === "admin" && (
+        <div className="mt-16">
+          {hasEditPermission ? (
+            <div className=" p-3 text-sm bg-green-500 flex flex-row justify-center items-center">
+              Hello there Admin, you can edit this post.
+            </div>
+          ) : (
+            <div className=" p-3 text-sm bg-yellow-400 flex flex-row justify-center items-center">
+              You don't have permission to edit this.
+            </div>
+          )}
+        </div>
+      )}
+
+      <EditorNav
+        postStatus={postStatus}
+        isEditor={true}
+        editorButtons={
           <div className="my-auto flex mr-1">
-            {/* {editorType === "edit" && (
-              <div>
-                <button
-                  onClick={onExport}
-                  className="p-1 px-3 bg-yellow-400 rounded text-sm text-white"
-                >
-                  Export
-                </button>
+            {hasUnsavedChanges && (
+              <div className="inline mr-6 text-pink-500 text-xs my-auto">
+                Unsaved changes
               </div>
-            )} */}
-            {/* {editorType === "edit" && ( */}
+            )}
 
-            {hasUnsavedChanges && 
-            <div className="inline mr-6 text-pink-500 text-xs my-auto">
-              Unsaved changes
-              </div>
-              }
-              <div>
-                <Button
-                  variant="ghostBlue"
-                  onClick={onSave}
-                  className="text-sm"
-                >
-                  {saving ? "Saving..." : postStatus=='publish'?"Update":"Save Draft"}
-                </Button>
-              </div>
-            {/* )} */}
-            {(slug && postStatus!='publish') && <SubmitPostModal handleBeforeOpen={handleBeforeSubmit}>
-              <div className="p-10">
-                <div className="flex flex-col gap-4">
-                  <div className="flex flex-col gap-2">
-                    <h1 className="text-2xl my-0">
-                      Submit this post for review?
-                    </h1>
-
-                    <p className="text-sm leading-7 my-0 mb-4">
-                      Your story will be submitted to our publication editors for review. 
-                      The editors will review your draft and publish it within 24hrs if it fits our guidelines, or get back to you with feedback. 
-                      Readers will not see your story in the publication until it is reviewed and published by our editors.
-                      Feel free to continue editing even after submitting.
-                    </p>
-                  </div>
-                  {/* <div className="p-4">
-                    <img src="/static/images/source-bg.png" />
-                  </div> */}
-
+            <>
+              {mode !== "admin" && (
+                <div>
                   <Button
-                    onClick={onSubmit}
-                    className="px-3 py-2 text-md"
+                    variant="ghostBlue"
+                    onClick={onSave}
+                    className="text-sm"
                   >
-                    Submit
+                    {saving
+                      ? "Saving..."
+                      : postStatus == "publish"
+                      ? "Update"
+                      : "Save Draft"}
                   </Button>
                 </div>
-              </div>
-            </SubmitPostModal>}
+              )}
+
+              {mode === "admin" && hasEditPermission && (
+                <div>
+                  <Button
+                    variant="ghostBlue"
+                    onClick={onSave}
+                    className="text-sm"
+                  >
+                    {saving
+                      ? "Saving..."
+                      : postStatus == "publish"
+                      ? "Update"
+                      : "Save Draft"}
+                  </Button>
+                </div>
+              )}
+
+              {slug && postStatus != "publish" && mode !== "admin" && (
+                <SubmitPostModal handleBeforeOpen={handleBeforeSubmit}>
+                  <div className="p-10">
+                    <div className="flex flex-col gap-4">
+                      <div className="flex flex-col gap-2">
+                        <h1 className="text-2xl my-0">
+                          Submit this post for review?
+                        </h1>
+
+                        <p className="text-sm leading-7 my-0 mb-4">
+                          Your story will be submitted to our publication
+                          editors for review. The editors will review your draft
+                          and publish it within 24hrs if it fits our guidelines,
+                          or get back to you with feedback. Readers will not see
+                          your story in the publication until it is reviewed and
+                          published by our editors. Feel free to continue
+                          editing even after submitting.
+                        </p>
+                      </div>
+
+                      <Button onClick={onSubmit} className="px-3 py-2 text-md">
+                        Submit
+                      </Button>
+                    </div>
+                  </div>
+                </SubmitPostModal>
+              )}
+            </>
           </div>
-      }
+        }
       />
-    {/* NAVIGATION END */}
+      {/* NAVIGATION END */}
 
       <div className="my-4 pt-16 relative pb-10 blog-content">
         {/* <div className="fixed bottom-10 left-5 z-10 flex flex-row gap-[2px]">
           <Spinner size={"sm"} />
           <span className="m-0 p-0">Saving</span>
         </div> */}
-        {editor && 
-       <MenuFloating editor={editor}/>}
-        <TextMenu  editor={editor} />
-        {loading || !editorCreated ?
-          <div style={{maxWidth:'100%'}} className="mx-2 h-screen absolute top-0 left-0 flex flex-col justify-center w-screen">
+        {editor && mode !== "admin" && <MenuFloating editor={editor} />}
+        {mode !== "admin" && <TextMenu editor={editor} />}
+
+        {loading || !editorCreated ? (
+          <div
+            style={{ maxWidth: "100%" }}
+            className="mx-2 h-screen absolute top-0 left-0 flex flex-col justify-center w-screen"
+          >
             <div className="-mt-32 mx-auto text-blue-800 opacity-80">
-            <Spinner />
+              <Spinner />
+            </div>
           </div>
-          </div>:
+        ) : (
           <EditorContent editor={editor} />
-        }
-       
+        )}
+
         <div className="popup-modal mb-6 relative bg-white p-6 pt-3 rounded-lg w-full"></div>
       </div>
     </div>
