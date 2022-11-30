5c995a25331b629d348464c111c37bbac25a4e02
Graeme Fulton <graemefulton89@gmail.com>
2022-11-30 14:41:06 +0000

    jobs query updated to same like posts, so we can use static generitin

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -1,5 +1,8 @@
 import Layout from "@/components/layout-dashboard";
 import fetchJson from "@/lib/iron-session/fetchJson";
+
+import { getAllJobs } from "@/lib/api";
+
 import Link from "next/link";
 import { useEffect } from "react";
 const qs = require("qs");
@@ -7,29 +10,34 @@ var axios = require("axios");
 // import useUser from "@/lib/iron-session/useUser";
 import { useState } from "react";
 
+const PAGE_SIZE = 12;
+
+
 const JobPostCard = ({ job }) => {
-  const tags = job.tags.split(",");
+  // const tags = job.tags.split(",");
   const company = job?.company?.data?.attributes;
   return (
     <div className="w-full h-auto p-10 bg-white rounded-[10px] flex flex-row gap-5 border border-black border-opacity-10">
-      <div className="w-[100px] h-[100px] bg-[#CEE2FF] rounded"></div>
+      {job.companyLogo?
+      <img className="w-[100px] h-[100px] bg-[#CEE2FF] rounded" src={job.companyLogo}/>
+      :<div className="w-[100px] h-[100px] bg-[#CEE2FF] rounded"></div>}
       <div className="flex flex-col gap-3 ">
         <div className="flex flex-col gap-2">
           <div className="flex flex-col gap-0">
             <h1 className="text-lg m-0 inter-font font-medium">{job.title}</h1>
             <div className="flex flex-row gap-2 text-base inter-font font-regular text-[#A3ACC2]">
-              <span>{company?.name}</span>
+              <span>{job.companyName}</span>
               <span>•</span>
               <span>{job.location}</span>
             </div>
           </div>
           <div className="gap-2 flex flex-row">
-            {tags.map((x, i) => (
+            {job.skills?.map((x, i) => (
               <span
                 className="py-1 px-5 text-[12px] rounded-full bg-[#F2BFFF] text-sm "
                 key={i}
               >
-                {x}
+                {x.name}
               </span>
             ))}
           </div>
@@ -51,37 +59,18 @@ const JobPostCard = ({ job }) => {
   );
 };
 
-const Index = () => {
-  const [jobs, setJobs] = useState([]);
+const Index = ({jobs}) => {
 
-  useEffect(() => {
-    const fetch = async () => {
-      const query = qs.stringify(
-        {
-          populate: "*",
-        },
-        {
-          encodeValuesOnly: true, // prettify URL
-        }
-      );
-      let jobEntriesConfig = {
-        method: "get",
-        url: `${process.env.NEXT_PUBLIC_API_URL}/api/jobs?${query}`,
-        headers: {},
-      };
-      const { data } = await axios(jobEntriesConfig);
-      setJobs(data?.data);
-    };
-
-    fetch();
-  }, []);
   return (
     <Layout background="#EFF2F8">
       <div className=" w-full h-full ">
         <div className="w-full max-w-7xl mx-auto  h-full grid grid-cols-4 gap-5">
           <div className="w-full h-full col-start-1 col-end-4 flex flex-col gap-5  pb-10">
-            {jobs.map((job, i) => {
+            {/* {jobs.map((job, i) => {
               return <JobPostCard job={job.attributes} key={i} />;
+            })} */}
+            {jobs.map((job, i) => {
+              return <JobPostCard job={job} key={i} />;
             })}
           </div>
           <div className="w-full h-full col-start-4 col-end-5 ">
@@ -100,3 +89,20 @@ const Index = () => {
 };
 
 export default Index;
+
+export async function getStaticProps({ preview = null, params }) {
+  const pageSize = PAGE_SIZE;
+  const page = 0;
+  let allPosts =(await getAllJobs(preview, pageSize, page)) || [];
+ 
+  const pagination = allPosts?.meta?.pagination;
+  return {
+    props: { 
+      // jobs: allPosts?.data?allPosts.data:null, 
+      jobs: allPosts, 
+      preview, 
+      pagination:pagination?pagination:null
+    },
+    revalidate:20
+  };
+}
