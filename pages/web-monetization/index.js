ead9c7b22077d8ebbdade4944ad238335b56cc53
Graeme Fulton <graemefulton89@gmail.com>
2022-12-07 12:32:46 +0100

    #158 meta desc for webmon

--- pages/web-monetization/index.js
+++ pages/web-monetization/index.js
@@ -8,10 +8,18 @@ import WebStandard from "@/components/landing-pages/components/web-mon/webStanda
 import FooterCTA from "@/components/landing-pages/components/web-mon/footerCTA";
 import PayoutSection from "@/components/landing-pages/components/web-mon/payoutSection";
 
+
+const seo = {
+  title: "Web Monetization on Prototypr",
+  description:"Web Monetization provides an open, native, efficient, and automatic way to compensate creators through payment providers like Coil. Finally, an alternative to intrusive ads.",
+  url:"https://prototypr.io/web-monetization",
+  image:"https://prototypr.io/static/images/coins.png"
+}
+
 const Index = () => {
   return (
     <div className="bg-[#22AA79] overflow-hidden">
-      <Meta />
+      <Meta seo={seo}/>
       <Header />
       <Hero />
       <WebMonetizationSection />
