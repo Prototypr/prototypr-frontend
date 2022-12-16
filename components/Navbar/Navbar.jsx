9bb3155572dc0ed5dba60cb957ac0aedcc68b3df
Joel Benjamin <joelbenjamin093@gmail.com>
2022-12-16 16:49:21 +0530

    christmas stuff

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import MobileActiveLink from "@/components/Navbar/parts/MobileActiveLink";
 import WMButton from "./parts/WMButton";
+import { SnowMiddle } from "../xmas/snow";
 
 const WMCounter = dynamic(
   () => import("@/components/WebMonetization/Counter"),
@@ -30,89 +31,104 @@ export const HomePageNewNavBar = () => {
     setMobileNavOpen(!mobileNavOpen);
   };
   return (
-    <div className="sticky top-10 z-50 ">
-      <div className="w-[100%] max-w-[1400px] mx-auto h-auto bg-white bg-opacity-60 border border-black border-opacity-10 backdrop-blur-lg rounded-full z-50">
-        <div className="flex flex-row gap-2 p-2 px-6 justify-between">
-          <div className="flex  flex-shrink-0 items-center">
-            <Link href="/" as="/">
-              <>
-                {/* pantalla grande */}
-                <img
-                  className="block h-8 w-auto "
-                  src="/static/images/logo-small.svg"
-                  alt="Prototypr Logo"
-                />
-              </>
-            </Link>
-            <div className={`hidden md:block my-auto duration-300 ease-in-out`}>
-              <WMCounter />
-            </div>
-          </div>
-          <div className="hidden sm:flex flex-row gap-2 p-2">
-            {[
-              { label: "Home", url: "/" },
-              { label: "Topics", url: "/topics" },
-              { label: "Jobs", url: "/jobs" },
-              { label: "Toolbox", url: "/toolbox" },
-              { label: "Sponsor", url: "/sponsor" },
-            ].map((tab) => {
-              return (
-                <div>
-                  <Link href={tab.url}>
-                    <span
-                      className={`px-6 hover:bg-gray-200 py-2 text-center block font-inter tracking-tight font-normal cursor-pointer min-w-max cursor w-full text-base rounded-full bg-transparent text-gray-500  `}
-                    >
-                      {tab.label}
-                    </span>
-                  </Link>
-                </div>
-              );
-            })}
-            <div className=" flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <div className={`hidden mr-2 md:block my-auto`}>
-                <WMButton />
-              </div>
-              {/* <NavigationMenu></NavigationMenu> */}
-              <div className="relative ml-3">
-                <UserMenu userLoading={isLoading} user={user} />
-              </div>
-            </div>
-          </div>
-          <div className=" inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              onClick={toggleMobileNav}
-              className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
+    <div className="relative">
+      {/* <div className="absolute z-[60] hidden sm:block top-0 left-[10%]">
+        <SnowMiddle />
+      </div> */}
 
-              {mobileNavOpen ? (
-                <XIcon className="block h-6 w-6" aria-hidden="true" />
-              ) : (
-                <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-              )}
-            </button>
+      <div className="sticky top-10 z-50 ">
+        <div className="w-[100%] max-w-[1400px] mx-auto h-auto bg-white bg-opacity-60 border border-black border-opacity-10 backdrop-blur-lg rounded-full z-50">
+          <div className="flex flex-row gap-2 p-2 px-6 justify-between">
+            <div className="flex  flex-shrink-0 items-center">
+              <Link href="/" as="/">
+                <>
+                  {/* pantalla grande */}
+                  {/* <img
+                  className="block h-8 w-auto "
+                  src="/static/images/logo-small-xmas.svg"
+                  //   src="/static/images/logo-small.svg"
+                  alt="Prototypr Logo"
+                /> */}
+                  <img
+                    className="block h-10 w-auto mb-2 "
+                    src="/static/images/logo-small-xmas.svg"
+                    //   src="/static/images/logo-small.svg"
+                    alt="Prototypr Logo"
+                  />
+                </>
+              </Link>
+              <div
+                className={`hidden md:block my-auto duration-300 ease-in-out`}
+              >
+                <WMCounter />
+              </div>
+            </div>
+            <div className="hidden sm:flex flex-row gap-2 p-2">
+              {[
+                { label: "Home", url: "/" },
+                { label: "Topics", url: "/topics" },
+                { label: "Jobs", url: "/jobs" },
+                { label: "Toolbox", url: "/toolbox" },
+                { label: "Sponsor", url: "/sponsor" },
+              ].map((tab) => {
+                return (
+                  <div>
+                    <Link href={tab.url}>
+                      <span
+                        className={`px-6 hover:bg-gray-200 py-2 text-center block font-inter tracking-tight font-normal cursor-pointer min-w-max cursor w-full text-base rounded-full bg-transparent text-gray-500  `}
+                      >
+                        {tab.label}
+                      </span>
+                    </Link>
+                  </div>
+                );
+              })}
+              <div className=" flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className={`hidden mr-2 md:block my-auto`}>
+                  <WMButton />
+                </div>
+                {/* <NavigationMenu></NavigationMenu> */}
+                <div className="relative ml-3">
+                  <UserMenu userLoading={isLoading} user={user} />
+                </div>
+              </div>
+            </div>
+            <div className=" inset-y-0 left-0 flex items-center sm:hidden">
+              <button
+                type="button"
+                onClick={toggleMobileNav}
+                className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+
+                {mobileNavOpen ? (
+                  <XIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        className={`sm:hidden relative mt-5 rounded-lg bg-white z-[100] ${
-          !mobileNavOpen ? "h-0 overflow-hidden" : ""
-        }`}
-        id="mobile-menu"
-      >
-        <div className="space-y-1 px-2 pt-2 pb-3 text-sm font-inter">
-          <MobileActiveLink href={"/"}>Home</MobileActiveLink>
-          <MobileActiveLink href={"/topics"}>Topics</MobileActiveLink>
+        <div
+          className={`sm:hidden relative mt-5 rounded-lg bg-white z-[100] ${
+            !mobileNavOpen ? "h-0 overflow-hidden" : ""
+          }`}
+          id="mobile-menu"
+        >
+          <div className="space-y-1 px-2 pt-2 pb-3 text-sm font-inter">
+            <MobileActiveLink href={"/"}>Home</MobileActiveLink>
+            <MobileActiveLink href={"/topics"}>Topics</MobileActiveLink>
 
-          <MobileActiveLink href={"/toolbox"}>Toolbox</MobileActiveLink>
-          <MobileActiveLink href={"/jobs"}>Jobs</MobileActiveLink>
-          <MobileActiveLink href={"/sponsor"}>Sponsor</MobileActiveLink>
-          <MobileActiveLink href={"/web-monetization"}>
-            Web Monetization
-          </MobileActiveLink>
+            <MobileActiveLink href={"/toolbox"}>Toolbox</MobileActiveLink>
+            <MobileActiveLink href={"/jobs"}>Jobs</MobileActiveLink>
+            <MobileActiveLink href={"/sponsor"}>Sponsor</MobileActiveLink>
+            <MobileActiveLink href={"/web-monetization"}>
+              Web Monetization
+            </MobileActiveLink>
+          </div>
         </div>
       </div>
     </div>
