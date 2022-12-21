a8f9e061d160d2fd5a7e5c09befb66cb1b55e000
Graeme Fulton <graemefulton89@gmail.com>
2022-12-21 21:12:30 +0000

    invite button - expires in 7 days

--- components/landing-pages/components/common/button.js
+++ components/landing-pages/components/common/button.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 const CTAButton = () => {
   return (
     <div>
-      <button className="p-5 px-10 w-full max-w-[250px] text-[16px] rounded-full bg-[#195DE2] text-white font-medium font-inter border border-[#9DDBFD] border-opacity-40">
-        Request an Invite
-      </button>
+      <Link href="https://discord.gg/Q8whPJ5U">
+        <button className="p-5 px-10 w-full max-w-[250px] text-[16px] rounded-full bg-[#195DE2] text-white font-medium font-inter border border-[#9DDBFD] border-opacity-40">
+          Join on Discord
+        </button>
+      </Link>
     </div>
   );
 };
