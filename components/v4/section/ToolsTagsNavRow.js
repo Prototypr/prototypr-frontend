62012fd890a55703e39e0719e5ec699b5e89b9ef
Graeme Fulton <graemefulton89@gmail.com>
2023-01-10 12:40:31 +0100

    breadcrumb options

--- components/v4/section/ToolsTagsNavRow.js
+++ components/v4/section/ToolsTagsNavRow.js
@@ -7,12 +7,21 @@ const ToolsTagsNavRow = ({tags, active}) => {
   return (
     <Container maxWidth="max-w-[1320px]">
       <div className="flex flex-wrap">
+      <GiantTag active={active=='All'} link={`/toolbox/page/1`}>All</GiantTag>
         {ALL_SLUGS_GROUPS.map((tag, index) => {
           
           return (
             <>
-            {tag.subItems?
+            {tag.moreLink?
               <GiantTag active={active==tag.title} link={`${tag?.moreLink?.url || "/"}`}>{tag.title}</GiantTag>
+            :
+            tag.subItems?tag.subItems.map((tag,index)=>{
+              return(
+                <>
+                <GiantTag active={active==tag.title} link={`/toolbox/${tag?.tags[0]}/page/1`}>{tag.title}</GiantTag>
+                </>
+              )
+            })
             :''}
             </>)
         })}
