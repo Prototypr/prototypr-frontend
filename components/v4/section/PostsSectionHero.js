51a091a167d7d53107f0ba4169767b500b4beb23
Graeme Fulton <graeme@prototypr.io>
2024-01-31 01:35:20 +0100

    /posts/page/x page

--- components/v4/section/PostsSectionHero.js
+++ components/v4/section/PostsSectionHero.js
@@ -5,25 +5,26 @@ import PostsGroup3Cards from "../layout/PostsGroup3Cards";
 import RSSTitle from "../text/RSSTitle";
 import SectionDivider from "./SectionDivider";
 // import {RssSimple} from 'phosphor-react'
-const PostsSectionHero = ({user, heroCardPost, viewablePosts, jobsSidebar,title }) => {
+const PostsSectionHero = ({user, heroCardPost, viewablePosts, jobsSidebar,title, showRecent }) => {
   return (
     <Container maxWidth="max-w-[1320px]">
-      <div className="w-full shadow-md h-full grid grid-cols-12 flex justify-center bg-white rounded-3xl p-6 lg:p-6">
-        <div className={`w-full max-w-full flex flex-col gap-2 col-span-12 `}>
-          <PostsGridHero title={title} largePost={heroCardPost} smallPosts={viewablePosts} />
+      {showRecent && 
+      <>
+        <div className="w-full shadow-md h-full grid grid-cols-12 flex justify-center bg-white rounded-3xl p-6 lg:p-6">
+            <div className={`w-full max-w-full flex flex-col gap-2 col-span-12 `}>
+            <PostsGridHero title={title} largePost={heroCardPost} smallPosts={viewablePosts} />
+            </div>
         </div>
-        {/* <SidebarDiscover
-          paddingTop="hidden ml-4 pl-6 lg:block pt-12"
-          content={jobsSidebar}
-        /> */}
-      </div>
-      <SectionDivider py="py-4" transparentLine={true}/>
+        <SectionDivider py="py-4" transparentLine={true}/>
+      </>
+      
+      }
       <div className="w-full shadow-md h-full grid grid-cols-12 flex justify-center bg-white rounded-3xl p-6 lg:p-6">
         <div className={`w-full max-w-full flex flex-col gap-2 col-span-12 `}>
          <div className="flex justify-between">
             <RSSTitle title={'All posts'}/>
             </div>
-            <PostsGroup3Cards posts={viewablePosts?.slice(2,viewablePosts?.length)}/>
+            <PostsGroup3Cards posts={!showRecent?viewablePosts:viewablePosts?.slice(2,viewablePosts?.length)}/>
         </div>
         {/* <SidebarDiscover
           paddingTop="hidden ml-4 pl-6 lg:block pt-12"
