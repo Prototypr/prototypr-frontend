61223f47e7f94800961c1e3ac06be419a431675e
Graeme Fulton <graeme@prototypr.io>
2024-02-19 14:49:44 +0100

    strapi invite only part 1

--- components/icons/index.js
+++ components/icons/index.js
@@ -4,3 +4,109 @@ export const LocationIcon = ({className=defaultClass})=><svg className={`${class
 export const MoneyIcon = ({className=defaultClass})=><svg className={`${className}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-3.5-6H14a.5.5 0 1 0 0-1h-4a2.5 2.5 0 1 1 0-5h1V6h2v2h2.5v2H10a.5.5 0 1 0 0 1h4a2.5 2.5 0 1 1 0 5h-1v2h-2v-2H8.5v-2z" fill="currentColor"/></svg>
 export const ClockIcon = ({className=defaultClass})=><svg className={`${className}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-8h4v2h-6V7h2v5z" fill="currentColor"/></svg>
 export const CoinsIcon = ({className=defaultClass})=><svg className={`${className}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M14 2a8 8 0 0 1 3.292 15.293A8 8 0 1 1 6.706 6.707 8.003 8.003 0 0 1 14 2zm-4 6a6 6 0 1 0 0 12 6 6 0 0 0 0-12zm1 1v1h2v2H9a.5.5 0 0 0-.09.992L9 13h2a2.5 2.5 0 1 1 0 5v1H9v-1H7v-2h4a.5.5 0 0 0 .09-.992L11 15H9a2.5 2.5 0 1 1 0-5V9h2zm3-5a5.985 5.985 0 0 0-4.484 2.013 8 8 0 0 1 8.47 8.471A6 6 0 0 0 14 4z" fill="currentColor"/></svg>
+
+export const GoogleIcon = () =>{
+    return(
+        <svg
+
+        className="h-[24px] w-[24px] my-auto"
+        width={24}
+        height={24}
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 48 48"
+          >
+            <defs>
+              {" "}
+              <path
+                id="prefix__a"
+                d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+              />{" "}
+            </defs>{" "}
+            <clipPath id="prefix__b">
+              {" "}
+              <use xlinkHref="#prefix__a" overflow="visible" />{" "}
+            </clipPath>{" "}
+            <path
+              clipPath="url(#prefix__b)"
+              fill="#FBBC05"
+              d="M0 37V11l17 13z"
+            />{" "}
+            <path
+              clipPath="url(#prefix__b)"
+              fill="#EA4335"
+              d="M0 11l17 13 7-6.1L48 14V0H0z"
+            />{" "}
+            <path
+              clipPath="url(#prefix__b)"
+              fill="#34A853"
+              d="M0 37l30-23 7.9 1L48 0v48H0z"
+            />{" "}
+            <path
+              clipPath="url(#prefix__b)"
+              fill="#4285F4"
+              d="M48 48L17 24l-4-3 35-10z"
+            />{" "}
+          </svg>
+    )
+}
+
+export const GitHubIcon = () =>{
+    return(
+        <svg
+
+        className="h-[24px] w-[24px] my-auto"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 48 48"
+      >
+        <g data-name="Layer 2">
+          <g fill="none" data-name="invisible box">
+            <path d="M0 0H48V48H0z"></path>
+            <path d="M0 0H48V48H0z"></path>
+          </g>
+          <path
+            fill="#fff"
+            d="M24 1.9a21.6 21.6 0 00-6.8 42.2c1 .2 1.8-.9 1.8-1.8v-2.9c-6 1.3-7.9-2.9-7.9-2.9a6.5 6.5 0 00-2.2-3.2c-2-1.4.1-1.3.1-1.3a4.3 4.3 0 013.3 2c1.7 2.9 5.5 2.6 6.7 2.1a5.4 5.4 0 01.5-2.9C12.7 32 9 28 9 22.6a10.7 10.7 0 012.9-7.6 6.2 6.2 0 01.3-6.4 8.9 8.9 0 016.4 2.9 15.1 15.1 0 015.4-.8 17.1 17.1 0 015.4.7 9 9 0 016.4-2.8 6.5 6.5 0 01.4 6.4 10.7 10.7 0 012.8 7.6c0 5.4-3.7 9.4-10.5 10.6a5.4 5.4 0 01.5 2.9v6.2a1.8 1.8 0 001.9 1.8A21.7 21.7 0 0024 1.9z"
+            data-name="icons Q2"
+          ></path>
+        </g>
+      </svg>
+    )
+}
+
+export const EmailIcon = () =>{
+    return(
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        // className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
+        className="h-[24px] w-[24px] my-auto"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        />
+      </svg>
+    )
+}
+
+
+export const TicketIcon = ()=> {
+  return (
+    <svg
+    className="h-[24px] w-[24px] my-auto"
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 256 256"
+    >
+      <path fill="currentColor" d="M227.19 104.48A16 16 0 00240 88.81V64a16 16 0 00-16-16H32a16 16 0 00-16 16v24.81a16 16 0 0012.81 15.67 24 24 0 010 47A16 16 0 0016 167.19V192a16 16 0 0016 16h192a16 16 0 0016-16v-24.81a16 16 0 00-12.81-15.67 24 24 0 010-47zM32 167.2a40 40 0 000-78.39V64h56v128H32zm192 0V192H104V64h120v24.8a40 40 0 000 78.39z"></path>
+    </svg>
+  );
+}
--- components/sign-in/LoginForm.jsx
+++ components/sign-in/LoginForm.jsx
@@ -7,126 +7,188 @@ import axios from "axios";
 const Form = dynamic(() => import("@/components/Form"), { ssr: true });
 // const toast = dynamic(() => import('react-hot-toast'), { ssr: true })
 import toast from "react-hot-toast";
+import { EmailIcon, GitHubIcon, GoogleIcon, TicketIcon } from "../icons";
 
-const LoginForm = ({ isSignUp, title = "Sign up" }) => {
+const LoginForm = ({ isSignUp, title = "Sign up", user }) => {
+
+
+  const [inviteCode,setInviteCode] = useState(false);
+
+  return (
+    <div className="flex flex-col bg-[#fff] w-full rounded-3xl">
+      {isSignUp?
+      <InviteOnlyForm isSignUp={isSignUp} title={title} setInviteCode={setInviteCode}/>
+      
+    :  <ProviderForm isSignUp={isSignUp} title={title} inviteCode={inviteCode}/>
+    }
+    </div>
+  );
+};
+
+export default LoginForm;
+
+const InviteOnlyForm = () =>{
+
+  const [showInputForm, setShowInputForm] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
+
+return(
+  <div className="max-w-[90%] md:max-w-[480px] -mt-8 w-full mx-auto flex flex-col">
+
+  <div className="w-[140px] bg-white rounded-2xl mb-16 ">
+        <img
+              src={`/static/images/logo.svg`}
+              data-gumlet="false"
+              alt="Prototypr Logo"
+            />
+    </div>
+    <h2 className="text-3xl font-inter text-gray-800 font-semibold">
+      Create a new account
+    </h2>
+    <p className="my-4 mb-8">
+
+    Prototypr membership is invite-only. Reading content is open to everyone, but to create a profile and share tools, stories, and links, you need to be a member. Get an invite from current members or submit an application form to join.
+    </p>
+    {/* <div className="max-w-[90%] md:max-w-[340px] flex flex-col"> */}
+    {showInputForm == false ? (
+        <Button
+          type="submit"
+          variant={"confirmRounded"}
+          className="text-center !max-w-[320px] !w-full !py-2 !px-0 w-full !text-base !rounded-full font-normal"
+          onClick={() => {
+            setShowInputForm(!showInputForm);
+            // signIn("email", {email:'graeme@prototypr.io'})
+          }}
+        >
+          <div className="flex text-white">
+             <TicketIcon/>
+          <div className="ml-2.5 my-auto text-base inline-block ">
+           Join with Invite Code
+          </div>
+          </div>
+        </Button>
+      ) : (
+        <div className="max-w-xs w-full">
+          <Form
+            buttonText={`Submit`}
+            disabled={false}
+            disabledMessage={
+              <div className="text-center">
+                A login link has been sent to your email. <br />
+                If you didn't receive it,{" "}
+                <a
+                  className="text-blue-600 cursor-pointer"
+                  onClick={() => setSent(false)}
+                >
+                  retry
+                </a>
+                .
+              </div>
+            }
+            label={"Enter invite code"}
+            inputType={"text"}
+            placeholder={"XXX XXX XXX"}
+            isLoading={isLoading}
+            onSubmit={(e) => {
+              e.preventDefault();
+              setSent(false);
+              setIsLoading(true);
+              //create username from email
+              let code = e.target[0].value;
+              setInviteCode(code)
+           
+              var data = JSON.stringify({
+                invite_code:code
+              });
+
+              //@todo in strapi - add invite code collection
+              // change this to 'get' code in strapi
+              var config = {
+                method: "post",
+                url: `${process.env.NEXT_PUBLIC_API_URL}/api/passwordless/send-link`,
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                data: data,
+              };
+
+              const loadingToastId = toast.loading(
+                "Checking invite code..."
+              );
+
+              axios(config)
+                .then(function (response) {
+                  console.log(JSON.stringify(response.data));
+                  //redirect to verification page
+                  // Router.push('/account/verification-sent')
+                  // toast.success("Successfully updated", {
+                  //   duration: 10000,
+                  // });
+                  setSent(true);
+                  setTimeout(() => {
+                    setSent(true);
+                    setIsLoading(false);
+                    showSuccessToast(loadingToastId, 'accessCode');
+                  }, 800);
+                })
+                .catch(function (error) {
+                  setSent(false);
+                  setIsLoading(false);
+                  toast.dismiss(loadingToastId);
+                  alert("The invite code is invalid.");
+                });
+            }}
+          />
+        </div>
+      )}
+    {/* </div> */}
+  </div>
+)
+
+}
+
+const ProviderForm = ({ isSignUp, title = "Sign up", inviteCode })=>{
+
   const [showLoginForm, setShowLoginForm] = useState(false);
   const [sent, setSent] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <div className="flex flex-col bg-[#fff] rounded-3xl">
-        <div className="w-[140px] bg-white rounded-2xl mb-16 ">
-              <img
-                src={`/static/images/logo.svg`}
-                data-gumlet="false"
-                alt="Prototypr Logo"
-              />
-            </div>
+  return(
+    <div className="max-w-[90%] md:max-w-[320px] -mt-8 w-full mx-auto flex flex-col">
+
+      <div className="w-[140px] bg-white rounded-2xl mb-16 ">
+        <img
+          src={`/static/images/logo.svg`}
+          data-gumlet="false"
+          alt="Prototypr Logo"
+        />
+      </div>
       <h2 className="text-3xl font-inter text-gray-800 font-semibold">
         {isSignUp ? title : "Welcome back"}
       </h2>
       <div className="flex flex-col gap-4 flex-grow mt-6">
-        {/* <Button
-          isFullWidth
-          className="text-left w-full text-sm justify-start h-11 p-8 md:px-20 !rounded-full font-normal"
-          color="twitter"
-          leftIcon={
-            <div className="bg-white p-1 rounded-md">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#55ACEE"
-                viewBox="0 0 24 24"
-                className="!h-4 !w-4"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-              </svg>
-            </div>
-          }
-          onClick={() => signIn("twitter")}
-        >
-          {isSignUp ? "Sign up with Twitter" : "Sign in with Twitter"}
-        </Button> */}
-      
         <Button
           isFullWidth
           style={{border:'1px solid rgba(0,0,0,0.2)'}}
           variant={"confirmRounded"}
-          className="text-center w-full !py-2 !px-0 !bg-gray-50 !text-black/90 text-sm !rounded-full font-normal"
-          onClick={() => signIn("google")}
+          className="text-center !max-w-[320px] !w-full !mx-auto w-full !py-2 !px-0 !bg-gray-50 !text-black/90 !text-base !rounded-full font-normal"
+          onClick={() => signIn("google",  {invite_code: 'custominvitetoken'})}
         >
           <div className="flex">
-          <svg
-            width={24}
-            height={24}
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                viewBox="0 0 48 48"
-                // className="!h-4 !w-4"
-              >
-                <defs>
-                  {" "}
-                  <path
-                    id="prefix__a"
-                    d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
-                  />{" "}
-                </defs>{" "}
-                <clipPath id="prefix__b">
-                  {" "}
-                  <use xlinkHref="#prefix__a" overflow="visible" />{" "}
-                </clipPath>{" "}
-                <path
-                  clipPath="url(#prefix__b)"
-                  fill="#FBBC05"
-                  d="M0 37V11l17 13z"
-                />{" "}
-                <path
-                  clipPath="url(#prefix__b)"
-                  fill="#EA4335"
-                  d="M0 11l17 13 7-6.1L48 14V0H0z"
-                />{" "}
-                <path
-                  clipPath="url(#prefix__b)"
-                  fill="#34A853"
-                  d="M0 37l30-23 7.9 1L48 0v48H0z"
-                />{" "}
-                <path
-                  clipPath="url(#prefix__b)"
-                  fill="#4285F4"
-                  d="M48 48L17 24l-4-3 35-10z"
-                />{" "}
-              </svg>
+            <GoogleIcon/>
               <div className="ml-2.5 my-auto">
             {isSignUp ? "Sign up with Google" : "Sign in with Google"}
               </div>
           </div>
         </Button>
         <Button
-          isFullWidth
           variant={"confirmRounded"}
-          className="text-center !py-2 !px-0 w-full !bg-black/80 text-sm !rounded-full font-normal"
-          color="github"
+          className="text-center !max-w-[320px] !w-full !mx-auto !py-2 !px-0 w-full !bg-black/80 !text-base !rounded-full font-normal"
           onClick={() => signIn("github")}
         >
           <div className="flex">
 
-          <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 48 48"
-    >
-      <g data-name="Layer 2">
-        <g fill="none" data-name="invisible box">
-          <path d="M0 0H48V48H0z"></path>
-          <path d="M0 0H48V48H0z"></path>
-        </g>
-        <path
-          fill="#fff"
-          d="M24 1.9a21.6 21.6 0 00-6.8 42.2c1 .2 1.8-.9 1.8-1.8v-2.9c-6 1.3-7.9-2.9-7.9-2.9a6.5 6.5 0 00-2.2-3.2c-2-1.4.1-1.3.1-1.3a4.3 4.3 0 013.3 2c1.7 2.9 5.5 2.6 6.7 2.1a5.4 5.4 0 01.5-2.9C12.7 32 9 28 9 22.6a10.7 10.7 0 012.9-7.6 6.2 6.2 0 01.3-6.4 8.9 8.9 0 016.4 2.9 15.1 15.1 0 015.4-.8 17.1 17.1 0 015.4.7 9 9 0 016.4-2.8 6.5 6.5 0 01.4 6.4 10.7 10.7 0 012.8 7.6c0 5.4-3.7 9.4-10.5 10.6a5.4 5.4 0 01.5 2.9v6.2a1.8 1.8 0 001.9 1.8A21.7 21.7 0 0024 1.9z"
-          data-name="icons Q2"
-        ></path>
-      </g>
-    </svg>
+          <GitHubIcon/>
           <div className="ml-2.5 my-auto">
             {isSignUp ? "Sign up with GitHub" : "Sign in with GitHub"}
           </div>
@@ -138,31 +200,16 @@ const LoginForm = ({ isSignUp, title = "Sign up" }) => {
       </div>
       {showLoginForm == false ? (
         <Button
-          isFullWidth
           type="submit"
           variant={"confirmRounded"}
-          className="text-center !py-2 !px-0 w-full text-sm !rounded-full font-normal"
+          className="text-center !max-w-[320px] !w-full !mx-auto !py-2 !px-0 w-full !text-base !rounded-full font-normal"
           onClick={() => {
             setShowLoginForm(!showLoginForm);
             // signIn("email", {email:'graeme@prototypr.io'})
           }}
         >
           <div className="flex ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                // className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-                className="h-5 w-5"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                />
-              </svg>
+             <EmailIcon/>
           <div className="ml-2.5 my-auto inline-block ">
           {isSignUp ? "Sign up with Email" : "Sign in with Email"}
           </div>
@@ -214,6 +261,7 @@ const LoginForm = ({ isSignUp, title = "Sign up" }) => {
               var data = JSON.stringify({
                 email: e.target[0].value,
                 username: username,
+                invite_code:false
               });
 
               var config = {
@@ -253,14 +301,12 @@ const LoginForm = ({ isSignUp, title = "Sign up" }) => {
             }}
           />
         </div>
-      )}
-    </div>
-  );
-};
+      )}</div>
+  )
+  
+}
 
-export default LoginForm;
-
-const showSuccessToast = (loadingToastId) => {
+const showSuccessToast = (loadingToastId, type) => {
   toast.dismiss(loadingToastId);
   toast.custom(
     (t) => (
@@ -299,12 +345,19 @@ const showSuccessToast = (loadingToastId) => {
                 ></path>
               </svg>
             </div>
-            <div className="ml-3 overflow-hidden">
+            {type!=='accessCode'?<div className="ml-3 overflow-hidden">
               <p className="font-medium text-gray-900">Check your email.</p>
-              <p className="max-w-xs text-sm text-gray-500 truncate">
+              <p className="max-w-xs !text-base text-gray-500 truncate">
                 Click the link in your inbox to sign in.
               </p>
-            </div>
+            </div>:
+            <div className="ml-3 overflow-hidden">
+            <p className="font-medium text-gray-900">Access Code is valid.</p>
+            <p className="max-w-xs !text-base text-gray-500 truncate">
+              You can create your account.
+            </p>
+          </div>
+            }
           </div>
         </div>
       </div>
