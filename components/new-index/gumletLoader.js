b19f75d6eb54d7c92e0dce2094821ea399e9d3cf
Graeme Fulton <graeme@prototypr.io>
2024-05-02 22:59:19 +0100

    switch to avif and use srcset

--- components/new-index/gumletLoader.js
+++ components/new-index/gumletLoader.js
@@ -1,5 +1,5 @@
 const gumletLoader = ({ src, width, quality }) => {
-    return `${src}?w=${width}&q=${quality || 75}&format=auto&compress=true&dpr=2`
+    return `${src}?w=${width}&q=${quality || 70}&format=avif&compress=true&dpr=1`
   }
 
   export default gumletLoader
