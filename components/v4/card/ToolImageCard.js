b4b0eb6ece86265c5eadfbeca83c77a632784411
Graeme Fulton <graeme@prototypr.io>
2024-05-23 18:35:35 -0400

    remove log

--- components/v4/card/ToolImageCard.js
+++ components/v4/card/ToolImageCard.js
@@ -12,7 +12,6 @@ const ToolImageCard = ({
   navSponsor,
   sponsor,
 }) => {
-  console.log(navSponsor);
   return (
     <div
       className={`grid grid-cols-1 ${columns ? columns : "lg:grid-cols-3"} gap-4 w-full flex-wrap`}
